8ccf994b3388f8a4eb91306510f1d5c864b09112
petershih <petershih@synology.com>
2016-01-14 13:55:57 +0800

    implement

--- simulation/include/board.h
+++ simulation/include/board.h
@@ -1,8 +1,13 @@
 #ifndef _BOARD_H
 #define _BOARD_H
 
-#include "player.h"
-#include "opponent.h"
+#include <list>
+#include <vector>
+
+#include "card.h"
+#include "player-stat.h"
+#include "hidden-secret.h"
+#include "hidden-hand-card.h"
 
 class Board
 {
@@ -12,8 +17,16 @@ class Board
 		};
 
 	public:
-		Player player;
-		Opponent opponent;
+		PlayerStat player_stat;
+		std::list<Card> player_secrets;
+		std::list<Card> player_hand;
+		std::list<Card> player_deck;
+		std::list<Card> player_minions; // TODO: use a new 'Minion' type?
+
+		PlayerStat opponent_stat;
+		std::list<HiddenSecret> opponent_secrets;
+		std::list<HiddenHandCard> opponent_hand;
+		std::vector<Card> opponent_played_cards;
 
 		Turn turn;
 };
--- simulation/include/settings.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SETTINGS_H
-#define _SETTINGS_H
-
-#define MAX_SECRETS 5
-#define MAX_HAND    10
-#define MAX_DECK    64 // some spells put cards in deck
-
-#endif
