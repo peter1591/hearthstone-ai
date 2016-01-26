cf44aca6e912b6d96f3b04d818e4d9b068d11463
Peter Shih <petershih@synology.com>
2016-01-26 15:25:18 +0800

    Add comparator for Move class

--- game-engine/include/game-engine/move.h
+++ game-engine/include/game-engine/move.h
@@ -55,9 +55,12 @@ class Move
 			OpponentAttackData opponent_attack_data;
 
 			Data() {}
-		} data;
+		};
 
+	public:
 		Move() : action(ACTION_UNKNOWN) {}
+		bool operator==(const Move &rhs) const;
+		bool operator!=(const Move &rhs) const;
 
 		std::string GetDebugString() const;
 
@@ -69,7 +72,58 @@ class Move
 			game_flow_move.data.game_flow_data.rand_seed = rand_seed;
 			return game_flow_move;
 		}
+
+	public:
+		Data data;
 };
+
+inline bool Move::operator==(const Move &rhs) const
+{
+	if (this->action != rhs.action) return false;
+
+	switch (this->action) {
+	case ACTION_UNKNOWN:
+		break;
+
+	case ACTION_GAME_FLOW:
+		if (this->data.game_flow_data.rand_seed != rhs.data.game_flow_data.rand_seed) return false;
+		break;
+
+	case ACTION_PLAY_HAND_CARD_MINION:
+		if (this->data.play_hand_card_minion_data.idx_hand_card != rhs.data.play_hand_card_minion_data.idx_hand_card) return false;
+#ifdef CHOOSE_WHERE_TO_PUT_MINION
+		if (this->data.play_hand_card_minion_data.location != rhs.data.play_hand_card_minion_data.location) return false;
+#endif
+		break;
+
+	case ACTION_OPPONENT_PLAY_MINION:
+		if (this->data.opponent_play_minion_data.card != rhs.data.opponent_play_minion_data.card) return false;
+#ifdef CHOOSE_WHERE_TO_PUT_MINION
+		if (this->data.opponent_play_minion_data.location != rhs.data.opponent_play_minion_data.location) return false;
+#endif
+		break;
+
+	case ACTION_PLAYER_ATTACK:
+		if (this->data.player_attack_data.attacked_idx != rhs.data.player_attack_data.attacked_idx) return false;
+		if (this->data.player_attack_data.attacker_idx != rhs.data.player_attack_data.attacker_idx) return false;
+		break;
+
+	case ACTION_OPPONENT_ATTACK:
+		if (this->data.opponent_attack_data.attacked_idx != rhs.data.opponent_attack_data.attacked_idx) return false;
+		if (this->data.opponent_attack_data.attacker_idx != rhs.data.opponent_attack_data.attacker_idx) return false;
+		break;
+
+	case ACTION_END_TURN:
+		break;
+	}
+
+	return true;
+}
+
+inline bool Move::operator!=(const Move &rhs) const
+{
+	return !(*this == rhs);
+}
 
 } // namespace GameEngine
 
