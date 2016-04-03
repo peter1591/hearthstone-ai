db244af023a9187d7b2fce0f1edfeff6aac4652c
Peter Shih <petershih@synology.com>
2016-04-03 10:31:34 +0800

    Implement equality comparison for managed list

--- include/game-engine/managed-list/list.h
+++ include/game-engine/managed-list/list.h
@@ -5,6 +5,12 @@
 
 namespace GameEngine
 {
+	// Note:
+	// The iterator is missing by design.
+	// This container makes sure all the iterators instances (i.e., the ManagedItem) are valid
+	//      until the container vanishes.
+	// If a iterator class is introduced, we cannot guarantee the iterator's lifetime
+
 	template <typename ItemType>
 	class ManagedList
 	{
@@ -15,11 +21,22 @@ namespace GameEngine
 		ManagedList() {}
 		ManagedList(ManagedList<ItemType> && rhs)
 		{
-			this->items.splice(this->items.begin(), std::move(rhs)); // maintain the iterator (i.e., ManagedItem) validity
+			this->items.splice(this->items.begin(), std::move(rhs.items)); // maintain the iterator (i.e., ManagedItem) validity
 		}
 
-		ManagedItem PushFront(ItemType && item) { return ManagedItem(*this, this->items.push_front(item)); }
-		ManagedItem PushBack(ItemType && item) { return ManagedItem(*this, this->items.push_back(item)); }
+		bool operator==(ManagedList const& rhs) const { return this->items == rhs.items; }
+		bool operator!=(ManagedList const& rhs) const { return this->items != rhs.items; }
+
+		bool Empty() const { return this->items.empty(); }
+
+		ManagedItem PushFront(ItemType const& item) { return ManagedItem(this->items.insert(this->items.begin(), item)); }
+		ManagedItem PushFront(ItemType && item) { return ManagedItem(this->items.insert(this->items.begin(), std::move(item))); }
+
+		ManagedItem PushBack(ItemType const& item) { return ManagedItem(this->items.insert(this->items.end(), item)); }
+		ManagedItem PushBack(ItemType && item) { return ManagedItem(this->items.insert(this->items.end(), std::move(item))); }
+
+		// TODO: remove_if
+		// TODO: for_each
 
 	public: // only used by ManagedListIterator
 		void Erase(typename UnderlyingContainer::iterator it) { this->items.erase(it); }
