4bf583dcd6d50b4c17ae8a9ad345bad36339d601
Peter Shih <petershih@synology.com>
2016-04-04 17:59:42 +0800

    Arcane Explosion use new target framework

--- include/game-engine/cards/card_CS2_025.h
+++ include/game-engine/cards/card_CS2_025.h
@@ -9,16 +9,7 @@ static void Spell_Go(Player &player, SlotIndex)
 	constexpr bool damage_poisonous = false;
 	int damage = 1 + player.GetTotalSpellDamage();
 
-	// We need to get all minions at once, since a minion might summon another minion when the damage is dealt
-	// And by the game rule, the newly-summoned minion should not get damaged
-	std::list<MinionIterator> all_minions;
-	for (auto it = player.GetOppositePlayer().minions.GetIterator(0); !it.IsEnd(); it.GoToNext()) {
-		all_minions.push_back(it);
-	}
-
-	for (auto & minion : all_minions) {
-		StageHelper::DealDamage(*minion, damage, damage_poisonous);
-	}
+	StageHelper::DealDamage(BoardTargets(player.GetOppositePlayer().minions), damage, damage_poisonous);
 }
 
 DEFINE_CARD_CLASS_END()
