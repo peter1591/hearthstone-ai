5ca5a0fe70356830ed74f1a6546820fc46db8442
Peter Shih <petershih@synology.com>
2016-04-04 15:41:34 +0800

    Use another interface in stage helper
    - To elliminate the new instance of BoardObject

--- include/game-engine/cards/card_FP1_021.h
+++ include/game-engine/cards/card_FP1_021.h
@@ -4,7 +4,7 @@
 DEFINE_CARD_CLASS_START(FP1_021)
 // Weapon: Death's Bite
 			
-static void WeaponDeathrattle(GameEngine::Hero & equipped_hero)
+static void WeaponDeathrattle(Hero & equipped_hero)
 {
 	auto & board = equipped_hero.GetBoard();
 	constexpr bool damage_poisonous = false;
@@ -12,7 +12,7 @@ static void WeaponDeathrattle(GameEngine::Hero & equipped_hero)
 
 	// We need to get all minions at once, since a minion might summon another minion when the damage is dealt
 	// And by the game rule, the newly-summoned minion should not get damaged
-	std::list<GameEngine::MinionIterator> all_minions;
+	std::list<MinionIterator> all_minions;
 	for (auto it = board.player.minions.GetIterator(0); !it.IsEnd(); it.GoToNext()) {
 		all_minions.push_back(it);
 	}
@@ -21,13 +21,13 @@ static void WeaponDeathrattle(GameEngine::Hero & equipped_hero)
 	}
 
 	for (auto & target : all_minions) {
-		StageHelper::DealDamage(GameEngine::BoardObject(*target), damage, damage_poisonous);
+		StageHelper::DealDamage(*target, damage, damage_poisonous);
 	}
 }
 
-static void Weapon_AfterEquipped(GameEngine::Hero & equipped_hero)
+static void Weapon_AfterEquipped(Hero & equipped_hero)
 {
-	equipped_hero.AddWeaponOnDeathTrigger(GameEngine::Hero::WeaponOnDeathTrigger(WeaponDeathrattle));
+	equipped_hero.AddWeaponOnDeathTrigger(Hero::WeaponOnDeathTrigger(WeaponDeathrattle));
 }
 DEFINE_CARD_CLASS_END()
 #endif
