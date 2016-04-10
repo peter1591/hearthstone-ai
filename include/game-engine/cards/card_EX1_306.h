429e2e1c7eb8e310b8ea23a27bcb46e96d5b0b6c
Peter Shih <petershih@synology.com>
2016-04-10 12:10:18 +0800

    Fix bug

--- include/game-engine/cards/card_EX1_306.h
+++ include/game-engine/cards/card_EX1_306.h
@@ -8,8 +8,10 @@ static void BattleCry(Player & player, Move::PlayMinionData const& play_minion_d
 {
 	// discard a random card
 	// TODO: refactor out as a convenient function
-	int hand_card_idx = player.board.random_generator.GetRandom(player.hand.GetCount());
-	player.hand.DiscardHandCard(hand_card_idx);
+	if (player.hand.GetCount() > 0) {
+		int hand_card_idx = player.board.random_generator.GetRandom(player.hand.GetCount());
+		player.hand.DiscardHandCard(hand_card_idx);
+	}
 }
 
 DEFINE_CARD_CLASS_END()
