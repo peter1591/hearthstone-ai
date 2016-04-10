72b84d782a6a65e4c1058ef7f21b0e6a013d9711
Peter Shih <petershih@synology.com>
2016-04-11 01:22:33 +0800

    fix bug in card: Claw

--- include/game-engine/cards/card_CS2_005.h
+++ include/game-engine/cards/card_CS2_005.h
@@ -7,6 +7,7 @@ DEFINE_CARD_CLASS_START(CS2_005)
 static void Spell_Go(Player &player, SlotIndex target)
 {
 	player.enchantments.Add(std::make_unique<Enchantment_BuffPlayer_C<2, 0, true>>());
+	player.hero.GainArmor(2);
 }
 
 DEFINE_CARD_CLASS_END()
