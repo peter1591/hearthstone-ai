e5a351d300bc4304c34b1c69bab56d7ede3db4c8
Peter Shih <petershih@synology.com>
2016-04-11 13:41:36 +0800

    Add comments

--- include/game-engine/cards/card_example_minion.h
+++ include/game-engine/cards/card_example_minion.h
@@ -14,6 +14,12 @@ static void BattleCry(Player & player, Move::PlayMinionData const& play_minion_d
 
 static void AfterSummoned(MinionIterator summoned_minion)
 {
+	// Do NOT attach deathrattle to minion; the framework will automatically attach it.
+}
+
+static void Deathrattle(MinionIterator & triggering_minion)
+{
+
 }
 
 DEFINE_CARD_CLASS_END()
--- include/game-engine/cards/card_example_weapon.h
+++ include/game-engine/cards/card_example_weapon.h
@@ -14,7 +14,13 @@ static void Weapon_BattleCry(Player &equipping_player, SlotIndex target)
 
 static void Weapon_AfterEquipped(Hero & equipped_hero)
 {
+	// Attach deathrattle here (if any); the framework will not automatically attach it.
+	// TODO: attach automatically
 }
 
+static void WeaponDeathrattle(Hero & equipped_hero)
+{
+
+}
 
 DEFINE_CARD_CLASS_END()
