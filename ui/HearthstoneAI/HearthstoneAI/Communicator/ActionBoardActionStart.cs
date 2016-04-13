a9d769c5ee7f2fc93dc1bca6d91a48e2d1203f38
Peter Shih <petershih@synology.com>
2016-04-14 02:14:37 +0800

    Commit a missing file

---
+++ ui/HearthstoneAI/HearthstoneAI/Communicator/ActionBoardActionStart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace HearthstoneAI.Communicator
+{
+    [DataContract]
+    class ActionBoardActionStart : ActionBase
+    {
+        [DataMember]
+        Board.Game game;
+
+        public ActionBoardActionStart(int sequence, Board.Game game)
+            : base(sequence, "BoardActionStart")
+        {
+            this.game = game;
+        }
+    }
+}
