e913e6aae719d5e72adf3ab1b0e04a7557644e45
Peter Shih <petershih@synology.com>
2016-04-16 02:28:04 +0800

    Implement wild growth effect when crystal is full

--- include/game-engine/cards/card_CS2_013.h
+++ include/game-engine/cards/card_CS2_013.h
@@ -6,7 +6,26 @@ DEFINE_CARD_CLASS_START(CS2_013)
 
 static void Spell_Go(Player &player, SlotIndex target)
 {
-	player.stat.crystal.GainEmptyCrystals(1);
+	if (player.stat.crystal.GetTotal() < 10) {
+		player.stat.crystal.GainEmptyCrystals(1);
+	}
+	else {
+		if (player.hand.GetCount() < 10) {
+			player.hand.AddDeterminedCard(CardDatabase::GetInstance().GetCard(CARD_ID_CS2_013t));
+		}
+		else {
+			// should not happen?
+		}
+	}
+}
+
+DEFINE_CARD_CLASS_END()
+
+DEFINE_CARD_CLASS_START(CS2_013t)
+
+static void Spell_Go(Player &player, SlotIndex target)
+{
+	player.hand.DrawOneCardToHand();
 }
 
 DEFINE_CARD_CLASS_END()
