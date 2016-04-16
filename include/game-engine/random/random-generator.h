509dacf74fcd6d691b924a2e6e82acc5a03f1477
Peter Shih <petershih@synology.com>
2016-04-16 23:48:11 +0800

    Board use faster rand() for randoms within a stage

--- include/game-engine/random/random-generator.h
+++ include/game-engine/random/random-generator.h
@@ -3,7 +3,7 @@
 
 #include <cstdlib>
 #include <random>
-#include "common.h"
+#include "game-engine/common.h"
 
 #include <functional>
 
@@ -20,16 +20,15 @@ class RandomGenerator
 
 		void SetRandomSeed(unsigned int seed)
 		{
-			this->random_generator.seed(seed);
+			srand(seed);
 		}
 
 		unsigned int GetRandom(unsigned int exclusive_max) {
-			if (exclusive_max < 0) throw std::runtime_error("invalid argument");
-			else if (exclusive_max == 0) return this->random_generator();
+			if (exclusive_max <= 0) throw std::runtime_error("invalid argument");
 			else if (exclusive_max == 1) return 0;
 			else {
 				this->has_called = true;
-				return this->random_generator() % exclusive_max;
+				return rand() % exclusive_max;
 			}
 		}
 
@@ -37,7 +36,6 @@ class RandomGenerator
 		bool GetFlag_HasCalled() const { return this->has_called; }
 
 	private:
-		std::mt19937_64 random_generator;
 		bool has_called;
 };
 
