58ef918d7c5b594a849d23ede447bcda791f2b9b
Peter Shih <petershih@synology.com>
2016-04-21 23:03:49 +0800

    Implement aura for card: Healing Totem

--- include/game-engine/cards/card_NEW1_009.h
+++ include/game-engine/cards/card_NEW1_009.h
@@ -4,6 +4,26 @@ DEFINE_CARD_CLASS_START(NEW1_009)
 
 // Healing Totem
 
-// TODO: implement aura to heal friendly minions when end-turn
+class Aura : public MinionAura
+{
+public:
+	Aura(Minion & owner) : MinionAura(owner) {}
+
+public:
+	void TurnEnd(bool owner_turn)
+	{
+		if (owner_turn) {
+			// heal 1 for all friendly minions
+			for (auto it = this->GetOwner().GetMinions().GetIterator(0); !it.IsEnd(); it.GoToNext())
+			{
+				it->Heal(1);
+			}
+		}
+	}
+
+private: // for comparison
+	bool EqualsTo(HookListener const& rhs_base) const { return dynamic_cast<decltype(this)>(&rhs_base) != nullptr; }
+	std::size_t GetHash() const { return typeid(*this).hash_code(); }
+};
 
 DEFINE_CARD_CLASS_END()
