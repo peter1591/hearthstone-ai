3555373c3566d6e2b0a38f74fb3d36f9f1c11b0b
Peter Shih <petershih@synology.com>
2016-11-01 00:04:50 +0800

    Modify test code

--- dev/event-manager/Source.cpp
+++ dev/event-manager/Source.cpp
@@ -11,29 +11,59 @@ int main(void)
 	EventManager::Handlers::MinionSummoned handler1("a");
 	EventManager::Handlers::MinionSummoned handler2("b");
 
-	auto token1 = manager.PushBack(handler1);
-	auto token2 = manager.PushBack(handler2);
+	manager.PushBack(handler1);
+	manager.PushBack(handler2);
 
 	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
-
-	std::cout << "s1" << std::endl;
-	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
-
-	std::cout << "s2" << std::endl;
-	manager.Remove(token1);
-	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
-
-	std::cout << "s3" << std::endl;
-	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
-
-	std::cout << "s4" << std::endl;
-	auto token3 = manager.PushBack(handler1);
-	auto token4 = manager.PushBack(handler2);
-	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
+	manager.TriggerEvent<EventManager::Handlers::MinionSummonedOnce>();
 
 	std::cout << "===" << std::endl;
-	manager.Remove(token2);
 	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
+	manager.TriggerEvent<EventManager::Handlers::MinionSummonedOnce>();
+
+	std::cout << "===" << std::endl;
+	manager.PushBack(EventManager::Handlers::MinionSummonedOnce("once-a"));
+	manager.PushBack(EventManager::Handlers::MinionSummonedOnce("once-b"));
+	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
+	manager.TriggerEvent<EventManager::Handlers::MinionSummonedOnce>();
+
+	std::cout << "===" << std::endl;
+	manager.TriggerEvent<EventManager::Handlers::MinionSummoned>();
+	manager.TriggerEvent<EventManager::Handlers::MinionSummonedOnce>();
+
+	std::cout << "===" << std::endl;
+	manager.PushBack(3, EventManager::Handlers::MinionSummoned("cat-3.1"));
+	manager.PushBack(3, EventManager::Handlers::MinionSummoned("cat-3.2"));
+	manager.PushBack(9999, EventManager::Handlers::MinionSummoned("cat-9999.1"));
+	manager.PushBack(9999, EventManager::Handlers::MinionSummoned("cat-9999.2"));
+	manager.PushBack(3, EventManager::Handlers::MinionSummonedOnce("cat-3.3"));
+	manager.PushBack(3, EventManager::Handlers::MinionSummonedOnce("cat-3.4"));
+	manager.PushBack(9999, EventManager::Handlers::MinionSummonedOnce("cat-9999.3"));
+	manager.PushBack(9999, EventManager::Handlers::MinionSummonedOnce("cat-9999.4"));
+
+	std::cout << "===" << std::endl;
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummoned>(2);
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummonedOnce>(2);
+
+	std::cout << "===" << std::endl;
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummoned>(9999);
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummonedOnce>(9999);
+
+	std::cout << "===" << std::endl;
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummoned>(3);
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummonedOnce>(3);
+
+	std::cout << "===" << std::endl;
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummoned>(2);
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummonedOnce>(2);
+
+	std::cout << "===" << std::endl;
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummoned>(9999);
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummonedOnce>(9999);
+
+	std::cout << "===" << std::endl;
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummoned>(3);
+	manager.TriggerEvent<int, EventManager::Handlers::MinionSummonedOnce>(3);
 
 	return 0;
 }
