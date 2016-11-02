55a797a37be1be4cf0d9295acd9509ab5f0fcc96
Peter Shih <petershih@synology.com>
2016-11-02 21:06:01 +0800

    Test clone wrapper

--- dev/vs_projects/cloneable_containers/Source.cpp
+++ dev/vs_projects/cloneable_containers/Source.cpp
@@ -4,6 +4,7 @@
 
 #include "CloneableContainers/Vector.h"
 #include "CloneableContainers/RemovableVector.h"
+#include "CloneableContainers/CloneByCopyWrapper.h"
 
 static void test1()
 {
@@ -122,10 +123,53 @@ static void test3()
 	}
 }
 
+class Wrap1
+{
+public:
+	explicit Wrap1(std::unique_ptr<int> v) : v_(std::move(v)) {}
+
+	friend std::ostream& operator<<(std::ostream& os, const Wrap1 & v)
+	{
+		os << "[@" << v.v_.get() << "]: " << *v.v_;
+		return os;
+	}
+
+	Wrap1 Clone() const
+	{
+		return Wrap1(std::make_unique<int>(*this->v_));
+	}
+
+private:
+	std::unique_ptr<int> v_;
+};
+
+static void test4()
+{
+	Wrap1 rawitem1(std::make_unique<int>(2002));
+	CloneByCopyWrapper<Wrap1> item1(std::move(rawitem1));
+
+	CloneableContainers::RemovableVector<CloneByCopyWrapper<Wrap1>> vec1;
+	auto token1 = vec1.PushBack(std::move(item1));
+	std::cout << "==" << std::endl;
+	std::cout << "value: " << vec1.Get(token1)->Get() << std::endl;
+
+	auto vec2 = vec1;
+	std::cout << "==" << std::endl;
+	std::cout << "value: " << vec1.Get(token1)->Get() << std::endl;
+	std::cout << "value: " << vec2.Get(token1)->Get() << std::endl;
+
+	vec1.Remove(token1);
+	std::cout << "==" << std::endl;
+	std::cout << "value 1: @" << vec1.Get(token1) << std::endl;
+	std::cout << "value 2: @" << vec2.Get(token1) << std::endl;
+	std::cout << "value 2: " << vec2.Get(token1)->Get() << std::endl;
+}
+
 int main(void)
 {
 	//test1();
 	//test2();
-	test3();
+	//test3();
+	test4();
 	return 0;
 }
