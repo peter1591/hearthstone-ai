49068b25ab98d429be1689c3dbb60412e3dc612c
Peter Shih <petershih@synology.com>
2016-11-02 12:23:38 +0800

    Add unit test

--- dev/vs_projects/cloneable_containers/Source.cpp
+++ dev/vs_projects/cloneable_containers/Source.cpp
@@ -4,7 +4,7 @@
 
 #include "CloneableContainers/Vector.h"
 
-int main(void)
+static void test1()
 {
 	auto vec1 = std::make_unique<CloneableContainers::Vector<std::string>>();
 
@@ -15,6 +15,10 @@ int main(void)
 	std::cout << "token1_1 = " << vec1->Get(token1_1) << std::endl;
 	std::cout << "token1_2 = " << vec1->Get(token1_2) << std::endl;
 
+	for (auto it = vec1->GetFirst(); !vec1->ReachedEnd(it); vec1->StepNext(it)) {
+		std::cout << "Iterated vec1: " << vec1->Get(it) << std::endl;
+	}
+
 	std::cout << "==" << std::endl;
 	auto vec2 = *vec1;
 	auto token2_1 = token1_1;
@@ -23,11 +27,25 @@ int main(void)
 	std::cout << "token1_2 = " << vec1->Get(token1_2) << std::endl;
 	std::cout << "token2_1 = " << vec2.Get(token2_1) << std::endl;
 	std::cout << "token2_2 = " << vec2.Get(token2_2) << std::endl;
+	for (auto it = vec1->GetFirst(); !vec1->ReachedEnd(it); vec1->StepNext(it)) {
+		std::cout << "Iterated vec1: " << vec1->Get(it) << std::endl;
+	}
+	for (auto it = vec2.GetFirst(); !vec2.ReachedEnd(it); vec2.StepNext(it)) {
+		std::cout << "Iterated vec2: " << vec2.Get(it) << std::endl;
+	}
 
 	std::cout << "==" << std::endl;
 	vec1.reset();
 	std::cout << "token2_1 = " << vec2.Get(token2_1) << std::endl;
 	std::cout << "token2_2 = " << vec2.Get(token2_2) << std::endl;
+	for (auto it = vec2.GetFirst(); !vec2.ReachedEnd(it); vec2.StepNext(it)) {
+		std::cout << "Iterated vec2: " << vec2.Get(it) << std::endl;
+	}
 
+}
+
+int main(void)
+{
+	test1();
 	return 0;
 }
