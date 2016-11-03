4ad1f94abb74171a9cd19a78b41754e1a981b785
Peter Shih <petershih@synology.com>
2016-11-03 14:01:08 +0800

    Implement ptr clone wrapper

--- dev/include/CloneableContainers/CloneByCopyWrapper.h
+++ dev/include/CloneableContainers/CloneByCopyWrapper.h
@@ -24,3 +24,28 @@ public:
 private:
 	T item_;
 };
+
+template <typename T>
+class CopyByPtrCloneWrapper
+{
+public:
+	CopyByPtrCloneWrapper(T&& item) : item_(std::move(item)) {}
+	CopyByPtrCloneWrapper(const CopyByPtrCloneWrapper<T>& rhs)
+		: item_(rhs.item_->Clone())
+	{
+	}
+	CopyByPtrCloneWrapper(CopyByPtrCloneWrapper<T>&& rhs) = default;
+
+	CopyByPtrCloneWrapper& operator=(const CopyByPtrCloneWrapper<T>& rhs)
+	{
+		item_ = std::move(item_->Clone());
+		return *this;
+	}
+	CopyByPtrCloneWrapper& operator=(CopyByPtrCloneWrapper<T>&& rhs) = default;
+
+	T & Get() { return item_; }
+	const T & Get() const { return item_; }
+
+private:
+	T item_;
+};
