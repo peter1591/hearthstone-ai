47f09919e8b671b4cf92c76a9e760912613dd4a9
Peter Shih <petershih@synology.com>
2016-11-07 14:19:23 +0800

    Add test code

--- dev/vs_projects/entities_manager/Source.cpp
+++ dev/vs_projects/entities_manager/Source.cpp
@@ -5,27 +5,35 @@ int main(void)
 {
 	EntitiesManager mgr;
 
-	Card c1;
+	RawCard c1;
+	c1.card_type = kCardTypeMinion;
 	c1.card_id = "card_id_1";
-	CardRef r1 = mgr.PushBack(c1);
-	std::cout << "t1: " << mgr.Get(r1).card_id << std::endl;
+	c1.zone = kCardZoneDeck;
+	c1.cost = 5;
+	CardRef r1 = mgr.PushBack(Card(c1));
+	std::cout << "t1: " << mgr.Get(r1).GetZone() << std::endl;
 
-	c1.card_id = "card_id_111";
-	std::cout << "t2: " << mgr.Get(r1).card_id << std::endl;
+	c1.zone = kCardZoneGraveyard;
+	std::cout << "t2: " << mgr.Get(r1).GetZone() << std::endl;
 
 	std::cout << "==" << std::endl;
-	mgr.GetBasicManipulator(r1).ChangeCardId("card_id_123");
-	std::cout << mgr.Get(r1).card_id << std::endl;
+	mgr.GetMinionManipulator(r1).ChangeZone(kCardZoneHand);
+	std::cout << mgr.Get(r1).GetZone() << std::endl;
 
 	auto mgr2(mgr);
 	std::cout << "==" << std::endl;
-	std::cout << mgr.Get(r1).card_id << std::endl;
-	std::cout << mgr2.Get(r1).card_id << std::endl;
+	std::cout << mgr.Get(r1).GetZone() << std::endl;
+	std::cout << mgr2.Get(r1).GetZone() << std::endl;
 
-	mgr2.GetBasicManipulator(r1).ChangeCardId("card_id_234");
+	mgr2.GetMinionManipulator(r1).ChangeZone(kCardZonePlay);
 	std::cout << "==" << std::endl;
-	std::cout << mgr.Get(r1).card_id << std::endl;
-	std::cout << mgr2.Get(r1).card_id << std::endl;
+	std::cout << mgr.Get(r1).GetZone() << std::endl;
+	std::cout << mgr2.Get(r1).GetZone() << std::endl;
+
+	std::cout << "==" << std::endl;
+	mgr2.GetMinionManipulator(r1).SetCost(9);
+	std::cout << mgr.Get(r1).GetCost() << std::endl;
+	std::cout << mgr2.Get(r1).GetCost() << std::endl;
 
 	return 0;
 }
