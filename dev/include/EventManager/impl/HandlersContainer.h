c7e4a9f1fcb114b90aa364863cf3aee76f469eea
Peter Shih <petershih@synology.com>
2016-11-08 11:26:34 +0800

    Implement event resolving queue

--- dev/include/EventManager/impl/HandlersContainer.h
+++ dev/include/EventManager/impl/HandlersContainer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <list>
+#include <vector>
 #include <type_traits>
 #include <utility>
 
@@ -24,21 +25,41 @@ namespace EventManager
 				handlers_.push_back(handler);
 			}
 
+			// Note: Do not provide remove interface to outside
+
+			// Note: Do not provide insert interface to outside
+			//    Or, we need a new algo for TriggerAll()
+
 			template <typename... Args>
 			void TriggerAll(Args&&... args)
 			{
+				// We store all the triggers to a temporary queue, and this queue is immutable during resolve.
+				// Reference: http://hearthstone.gamepedia.com/Advanced_rulebook
+				//   "Rule 2c: A Queue becomes immutable once Hearthstone starts to resolve the first entry in it. No new entries can be added to the Queue after this point."
+
+				// Example:
+				//    If you play a spell, Troggzor the Earthinator summons a Burly Rockjaw Trogg.
+				//    The Burly Rockjaw Trogg does not trigger from the same spell because the consequences of playing the spell have already begun resolving.
+
+				if (handlers_.empty()) return;
+
 				auto it = handlers_.begin();
-				while (it != handlers_.end()) {
+
+				// Since we don't have insert method to outsiders, we can just wrtie down the last iterator to specify the temporary queue
+				auto it_last = handlers_.end();
+				--it_last;
+
+				while (true)
+				{
+					const bool exiting = (it == it_last);
 					HandlersContainerController controller;
 
 					it->Handle(controller, std::forward<Args>(args)...);
 
-					if (controller.IsRemoved()) {
-						it = handlers_.erase(it);
-					}
-					else {
-						++it;
-					}
+					if (controller.IsRemoved()) { it = handlers_.erase(it); }
+					else { ++it; }
+
+					if (exiting) break;
 				}
 			}
 
