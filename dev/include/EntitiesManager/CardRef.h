3facc079871bb36f13c0f613dc41d43c262b1e09
Peter Shih <petershih@synology.com>
2016-11-10 01:38:36 +0800

    Make CardRef hashable

--- dev/include/EntitiesManager/CardRef.h
+++ dev/include/EntitiesManager/CardRef.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <type_traits>
 #include "Entity/Card.h"
 #include "CloneableContainers/Vector.h"
 
@@ -8,5 +9,29 @@ class CardRef
 public:
 	explicit CardRef(typename CloneableContainers::Vector<Entity::Card>::Identifier id) : id(id) {}
 
+	template <typename T>
+	bool operator==(T&& rhs) const
+	{
+		static_assert(std::is_same<std::decay_t<T>, CardRef>::value, "Wrong type");
+		return this->id == rhs.id;
+	}
+
+	template <typename T> bool operator!=(T&& rhs) const
+	{
+		return !(*this == rhs);
+	}
+
 	typename CloneableContainers::Vector<Entity::Card>::Identifier id;
 };
+
+namespace std
+{
+	template <>
+	struct hash<CardRef>
+	{
+		std::size_t operator()(const CardRef& key) const
+		{
+			return CloneableContainers::Vector<Entity::Card>::IdentifierHasher()(key.id);
+		}
+	};
+}
