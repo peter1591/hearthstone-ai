c33b1997691822e042f3e31bfae505a001dcc9b7
Peter Shih <petershih@synology.com>
2016-11-11 00:31:30 +0800

    Add player identifier field

--- dev/include/Entity/EnchantableStates.h
+++ dev/include/Entity/EnchantableStates.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Entity/CardZone.h"
+#include "State/PlayerIdentifier.h"
 
 namespace Entity
 {
@@ -9,6 +10,7 @@ namespace Entity
 	public:
 		EnchantableStates() : zone(kCardZoneInvalid), cost(-1), attack(-1), max_hp(-1) {}
 
+		State::PlayerIdentifier player;
 		CardZone zone;
 		int cost;
 		int attack;
---
+++ dev/include/State/PlayerIdentifier.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace State
+{
+	enum PlayerIdentifier
+	{
+		kPlayerFirst,
+		kPlayerSecond
+	};
+}
