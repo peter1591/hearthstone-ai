c05480e81312b649f8ebf7dd5848fc1a87949db2
Peter Shih <petershih@synology.com>
2016-11-12 13:13:13 +0800

    Implement player cardref update callback

--- dev/include/Manipulators/Helpers/ZoneChanger.h
+++ dev/include/Manipulators/Helpers/ZoneChanger.h
@@ -34,8 +34,14 @@ namespace Manipulators
 			{
 				switch (card_.GetZone())
 				{
+				case Entity::kCardZoneDeck:
+					return AddToDeckZone(state);
+				case Entity::kCardZoneHand:
+					return AddToHandZone(state);
 				case Entity::kCardZonePlay:
 					return AddToPlayZone(state);
+				case Entity::kCardZoneGraveyard:
+					return AddToGraveyardZone(state);
 				}
 			}
 
@@ -44,13 +50,52 @@ namespace Manipulators
 			{
 				switch (card_.GetZone())
 				{
+				case Entity::kCardZoneDeck:
+					return RemoveFromDeckZone(state);
+				case Entity::kCardZoneHand:
+					return RemoveFromHandZone(state);
 				case Entity::kCardZonePlay:
 					return RemoveFromPlayZone(state);
+				case Entity::kCardZoneGraveyard:
+					return RemoveFromGraveyardZone(state);
 				}
+			}
+
+			void AddToDeckZone(State::State & state)
+			{
+				State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+				player.deck_.GetLocationManipulator().Insert(mgr_, card_ref_);
+			}
+			void RemoveFromDeckZone(State::State & state)
+			{
+				State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+				player.deck_.GetLocationManipulator().Remove(mgr_, card_.GetZonePosition());
+			}
+
+			void AddToHandZone(State::State & state)
+			{
+				State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+				player.hand_.GetLocationManipulator().Insert(mgr_, card_ref_);
+			}
+			void RemoveFromHandZone(State::State & state)
+			{
+				State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+				player.hand_.GetLocationManipulator().Remove(mgr_, card_.GetZonePosition());
 			}
 
 			void AddToPlayZone(State::State & state);
 			void RemoveFromPlayZone(State::State & state);
+
+			void AddToGraveyardZone(State::State & state)
+			{
+				State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+				player.graveyard_.GetLocationManipulator<ChangingCardType>().Insert(mgr_, card_ref_);
+			}
+			void RemoveFromGraveyardZone(State::State & state)
+			{
+				State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+				player.graveyard_.GetLocationManipulator<ChangingCardType>().Remove(mgr_, card_.GetZonePosition());
+			}
 
 		private:
 			EntitiesManager & mgr_;
@@ -65,13 +110,38 @@ namespace Manipulators
 
 			player.minions_.GetLocationManipulator().Insert(mgr_, card_ref_);
 		}
-
 		template <>
 		void ZoneChanger<Entity::kCardTypeMinion>::RemoveFromPlayZone(State::State & state)
 		{
 			State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
 
 			player.minions_.GetLocationManipulator().Remove(mgr_, card_.GetZonePosition());
+		}
+
+		template <>
+		void ZoneChanger<Entity::kCardTypeWeapon>::AddToPlayZone(State::State & state)
+		{
+			State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+			player.weapon_.Equip(card_ref_);
+		}
+		template <>
+		void ZoneChanger<Entity::kCardTypeWeapon>::RemoveFromPlayZone(State::State & state)
+		{
+			State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+			player.weapon_.Destroy();
+		}
+
+		template <>
+		void ZoneChanger<Entity::kCardTypeSecret>::AddToPlayZone(State::State & state)
+		{
+			State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+			player.secrets_.Add(card_.GetCardId(), card_ref_);
+		}
+		template <>
+		void ZoneChanger<Entity::kCardTypeSecret>::RemoveFromPlayZone(State::State & state)
+		{
+			State::Player & player = state.players.Get(card_.GetPlayerIdentifier());
+			player.secrets_.Remove(card_.GetCardId());
 		}
 	}
 }
