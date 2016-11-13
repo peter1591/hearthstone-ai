e8626957f8c31dd45e91920b577b2426564da464
Peter Shih <petershih@synology.com>
2016-11-14 01:33:10 +0800

    Deck track change id

--- dev/include/State/Deck.h
+++ dev/include/State/Deck.h
@@ -6,24 +6,62 @@
 #include "Entity/Card.h"
 #include "State/Utils/OrderedCardsManager.h"
 
+namespace Manipulators
+{
+	namespace Helpers
+	{
+		template <Entity::CardType T> class ZoneChanger;
+	}
+}
+
 namespace State
 {
 	class Deck
 	{
 	public:
-		Deck()
+		class LocationManipulator
+		{
+			template <Entity::CardType T> friend class Manipulators::Helpers::ZoneChanger;
+
+		public:
+			explicit LocationManipulator(Deck & deck) : deck_(deck) {}
+
+		private:
+			template <typename T1, typename T2>
+			void Insert(T1&& mgr, T2&& card_ref)
+			{
+				++deck_.change_id;
+				auto instance = Utils::OrderedCardsManager(deck_.cards_);
+				return instance.Insert(std::forward<T1>(mgr), std::forward<T2>(card_ref));
+			}
+			template <typename T1, typename T2>
+			void Remove(T1&& mgr, T2&& pos)
+			{
+				++deck_.change_id;
+				auto instance = Utils::OrderedCardsManager(deck_.cards_);
+				return instance.Remove(std::forward<T1>(mgr), std::forward<T2>(pos));
+			}
+
+		private:
+			Deck & deck_;
+		};
+		friend class LocationManipulator;
+
+	public:
+		Deck() : change_id(0)
 		{
 			cards_.reserve(40);
 		}
 
-		Utils::OrderedCardsManager GetLocationManipulator()
+		LocationManipulator GetLocationManipulator()
 		{
-			return Utils::OrderedCardsManager(cards_);
+			return LocationManipulator(*this);
 		}
 
 		size_t Size() const { return cards_.size(); }
 
 	private:
+		int change_id;
 		std::vector<CardRef> cards_;
 	};
 }
