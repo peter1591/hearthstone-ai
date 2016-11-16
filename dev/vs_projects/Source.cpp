3010398725339004765d7302396f979049770894
Peter Shih <petershih@synology.com>
2016-11-17 00:57:58 +0800

    Modify unit test for event manager

--- dev/vs_projects/Source.cpp
+++ dev/vs_projects/Source.cpp
@@ -11,135 +11,85 @@ static void test1()
 
 	std::function<void(EventManager::HandlersContainerController &, int)> callback1 =
 		[](EventManager::HandlersContainerController & controller, int v) {
-		std::cout << "hello. v = " << v << std::endl;
+		std::cout << "callback1. v = " << v << std::endl;
+	};
+	std::function<void(EventManager::HandlersContainerController &, int)> callback2 =
+		[](EventManager::HandlersContainerController & controller, int v) {
+		std::cout << "callback2. v = " << v << std::endl;
+	};
+	std::function<void(EventManager::HandlersContainerController &, int)> callback3 =
+		[](EventManager::HandlersContainerController & controller, int v) {
+		std::cout << "callback3. v = " << v << std::endl;
+		controller.Remove();
 	};
 
-	mgr.PushBack(EventManager::TriggerTypes::MinionSummoned(callback1));
-	mgr.PushBack(EventManager::TriggerTypes::MinionSummoned(callback1));
-	mgr.PushBack(EventManager::TriggerTypes::MinionSummoned(callback1));
+	mgr.PushBack(EventManager::TriggerTypes::AfterMinionSummoned(callback3));
+	mgr.PushBack(EventManager::TriggerTypes::AfterMinionSummoned(callback1));
+	mgr.PushBack(EventManager::TriggerTypes::AfterMinionSummoned(callback1));
+	mgr.PushBack(EventManager::TriggerTypes::AfterMinionSummoned(callback2));
+	mgr.PushBack(EventManager::TriggerTypes::BeforeMinionSummoned(callback2));
+	mgr.PushBack(EventManager::TriggerTypes::BeforeMinionSummoned(callback1));
+	mgr.PushBack(EventManager::TriggerTypes::BeforeMinionSummoned(callback3));
+	mgr.PushBack(EventManager::TriggerTypes::BeforeMinionSummoned(callback2));
 
-	mgr.PushBack(5, EventManager::TriggerTypes::MinionSummoned(callback1));
-	mgr.PushBack(55, EventManager::TriggerTypes::MinionSummoned(callback1));
-	mgr.PushBack(55, EventManager::TriggerTypes::MinionSummoned(callback1));
+	mgr.PushBack(5, EventManager::TriggerTypes::AfterMinionSummoned(callback3));
+	mgr.PushBack(5, EventManager::TriggerTypes::AfterMinionSummoned(callback1));
+	mgr.PushBack(5, EventManager::TriggerTypes::AfterMinionSummoned(callback2));
+	mgr.PushBack(55, EventManager::TriggerTypes::AfterMinionSummoned(callback1));
+	mgr.PushBack(55, EventManager::TriggerTypes::AfterMinionSummoned(callback3));
+	mgr.PushBack(55, EventManager::TriggerTypes::AfterMinionSummoned(callback2));
 
 	auto mgr2 = mgr;
 
-	EventManager::Event<EventManager::TriggerTypes::MinionSummoned> ev1(987);
+	EventManager::Event<EventManager::TriggerTypes::AfterMinionSummoned> ev1(987);
+	EventManager::Event<EventManager::TriggerTypes::BeforeMinionSummoned> ev2(98789);
+	typedef EventManager::StaticEvent<EventManager::TriggerTypes::AfterMinionSummoned> EV1;
+	typedef EventManager::StaticEvent<EventManager::TriggerTypes::BeforeMinionSummoned> EV2;
+
 	std::cout << "--" << std::endl;
 	ev1.TriggerEvent(mgr);
+	std::cout << "--" << std::endl;
+	EV1::TriggerEvent(mgr, 777);
+	std::cout << "--" << std::endl;
+	EV2::TriggerEvent(mgr, 7717);
+	std::cout << "--" << std::endl;
+	ev2.TriggerEvent(mgr);
 
-	/*
 	std::cout << "--" << std::endl;
-	ev2.TriggerEvent(mgr);
-	std::cout << "--" << std::endl;
-	ev2.TriggerEvent(mgr);
+	EV1::TriggerEvent(mgr2, 78877);
 	std::cout << "--" << std::endl;
 	ev1.TriggerEvent(mgr2);
 	std::cout << "--" << std::endl;
 	ev2.TriggerEvent(mgr2);
 	std::cout << "--" << std::endl;
-	ev2.TriggerEvent(mgr2);
+	EV2::TriggerEvent(mgr, 89797);
 
-	EventManager::CategorizedEvent<EventManager::Handlers::MinionSummoned> ev3(4, 199, "kkk", 9.64);
-	EventManager::CategorizedEvent<EventManager::Handlers::MinionSummoned> ev4(55, 199, "kkk", 123.656);
-	EventManager::CategorizedEvent<EventManager::Handlers::MinionSummonedOnce> ev5(55);
+	EventManager::CategorizedEvent<EventManager::TriggerTypes::AfterMinionSummoned> ev3(4, 199);
+	EventManager::CategorizedEvent<EventManager::TriggerTypes::AfterMinionSummoned> ev4(55, 1999);
+	typedef EventManager::StaticCategorizedEvent<EventManager::TriggerTypes::AfterMinionSummoned> EV3;
+
+	std::cout << "--" << std::endl;
+	EV3::TriggerEvent(mgr, 4, 999);
 	std::cout << "--" << std::endl;
 	ev3.TriggerEvent(mgr);
 	std::cout << "--" << std::endl;
+	EV3::TriggerEvent(mgr, 55, 12345);
+	std::cout << "--" << std::endl;
 	ev4.TriggerEvent(mgr);
-	std::cout << "--" << std::endl;
-	ev5.TriggerEvent(mgr);
-	std::cout << "--" << std::endl;
-	ev5.TriggerEvent(mgr);
+
 	std::cout << "---" << std::endl;
 	ev3.TriggerEvent(mgr2);
 	std::cout << "---" << std::endl;
+	EV3::TriggerEvent(mgr2, 4, 999);
+	std::cout << "---" << std::endl;
 	ev4.TriggerEvent(mgr2);
 	std::cout << "---" << std::endl;
-	ev5.TriggerEvent(mgr2);
-	std::cout << "---" << std::endl;
-	ev5.TriggerEvent(mgr2);
-	
-	std::cout << "--" << std::endl;
-	mgr.PushBack(55, handler3);
-	ev5.TriggerEvent(mgr);
-	std::cout << "---" << std::endl;
-	ev5.TriggerEvent(mgr2);
-
-	std::cout << "==" << std::endl;
-	*/
+	EV3::TriggerEvent(mgr2, 55, 112592);
 }
-/*
-
-static void test2()
-{
-	EventManager::HandlersManager mgr;
-
-	EventManager::Handlers::MinionSummoned handler1("a");
-	EventManager::Handlers::MinionSummoned handler2("b");
-	EventManager::Handlers::MinionSummonedOnce handler3("once-c");
-
-	mgr.PushBack(handler1);
-	mgr.PushBack(handler2);
-	mgr.PushBack(handler3);
-
-	mgr.PushBack(5, handler1);
-	mgr.PushBack(5, handler2);
-	mgr.PushBack(55, handler1);
-	mgr.PushBack(55, handler2);
-	mgr.PushBack(55, handler3);
-
-	auto mgr2 = mgr;
-
-	typedef EventManager::StaticEvent<EventManager::Handlers::MinionSummoned> EV1;
-	typedef EventManager::StaticEvent<EventManager::Handlers::MinionSummonedOnce> EV2;
-	std::cout << "--" << std::endl;
-	EV1::TriggerEvent(mgr, 987, "h199", 3.15);
-	std::cout << "--" << std::endl;
-	EV2::TriggerEvent(mgr);
-	std::cout << "--" << std::endl;
-	EV2::TriggerEvent(mgr);
-	std::cout << "--" << std::endl;
-	EV1::TriggerEvent(mgr2, 987, "h199", 3.15);
-	std::cout << "--" << std::endl;
-	EV2::TriggerEvent(mgr2);
-	std::cout << "--" << std::endl;
-	EV2::TriggerEvent(mgr2);
-
-	typedef EventManager::StaticCategorizedEvent<EventManager::Handlers::MinionSummoned> EV3;
-	typedef EventManager::StaticCategorizedEvent<EventManager::Handlers::MinionSummoned> EV4;
-	typedef EventManager::StaticCategorizedEvent<EventManager::Handlers::MinionSummonedOnce> EV5;
-	std::cout << "--" << std::endl;
-	EV3::TriggerEvent(mgr, 4, 199, "kkk", 9.64);
-	std::cout << "--" << std::endl;
-	EV4::TriggerEvent(mgr, 55, 199, "kkk", 123.656);
-	std::cout << "--" << std::endl;
-	EV5::TriggerEvent(mgr, 55);
-	std::cout << "--" << std::endl;
-	EV5::TriggerEvent(mgr, 55);
-	std::cout << "---" << std::endl;
-	EV3::TriggerEvent(mgr2, 4, 199, "kkk", 9.64);
-	std::cout << "---" << std::endl;
-	EV4::TriggerEvent(mgr2, 55, 199, "kkk", 123.656);
-	std::cout << "---" << std::endl;
-	EV5::TriggerEvent(mgr2, 55);
-	std::cout << "---" << std::endl;
-	EV5::TriggerEvent(mgr2, 55);
-
-	std::cout << "--" << std::endl;
-	mgr.PushBack(55, handler3);
-	EV5::TriggerEvent(mgr, 55);
-	std::cout << "---" << std::endl;
-	EV5::TriggerEvent(mgr2, 55);
-
-	std::cout << "==" << std::endl;
-}
-*/
 
 int main(void)
 {
 	test1();
-	//test2();
 
 	return 0;
 }
