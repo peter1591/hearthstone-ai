e34e2887e3a6e3411c25e62b36d240a6d52a3e02
Peter Shih <petershih@synology.com>
2016-11-18 01:00:58 +0800

    Add unit test for minion card dispatcher

--- dev/vs_projects/StaticDispatcher/Source.cpp
+++ dev/vs_projects/StaticDispatcher/Source.cpp
@@ -1,33 +1,12 @@
 #include <string>
 #include <iostream>
 
-#include "StaticDispatcher/Dispatcher.h"
-
-int v;
-
-class Client1
-{
-public:
-	static void BattleCry(const std::string& s)
-	{
-		v = 3;
-		//std::cout << "Client1::BattleCry() called: " << s << std::endl;
-	}
-};
-
-template <> struct StaticDispatcher::DispatcherMap<0> { typedef Client1 type; };
+#include "Cards/Minions/Dispatcher.h"
 
 int main(void)
 {
-	StaticDispatcher::Dispatcher dispatcher;
-
-	v = 0;
-
-	dispatcher.BattleCry(3, "asd");
-	std::cout << v << std::endl;
-
-	dispatcher.BattleCry(0, "asdasd");
-	std::cout << v << std::endl;
+	Cards::Minions::Dispatcher::BattleCry(2, "jjj");
+	Cards::Minions::Dispatcher::BattleCry(7, "jssjj");
 
 	return 0;
 }
