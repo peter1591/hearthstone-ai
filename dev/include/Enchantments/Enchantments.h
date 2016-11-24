1dbc9884c3be0c9f6a40cd28c9041be34644e6be
Peter Shih <petershih@synology.com>
2016-11-24 13:19:50 +0800

    Move enchantments to namespace State::Cards

--- dev/include/Enchantments/Enchantments.h
+++ dev/include/Enchantments/Enchantments.h
@@ -10,35 +10,35 @@ namespace State
 	namespace Cards
 	{
 		class Card;
+
+		class Enchantments
+		{
+		public:
+			typedef std::function<void(Card &)> ApplyFunctor;
+			typedef CloneableContainers::RemovableVector<ApplyFunctor> ContainerType;
+
+			template <typename T>
+			typename ContainerType::Identifier PushBack(T && item)
+			{
+				return enchantments_.PushBack(item.apply_functor);
+			}
+
+			template <typename T>
+			void Remove(T&& id)
+			{
+				return enchantments_.Remove(std::forward<T>(id));
+			}
+
+			void ApplyAll(Card & card)
+			{
+				enchantments_.IterateAll([&card](ApplyFunctor& functor) -> bool {
+					functor(card);
+					return true;
+				});
+			}
+
+		private:
+			ContainerType enchantments_;
+		};
 	}
 }
-
-class Enchantments
-{
-public:
-	typedef std::function<void(State::Cards::Card &)> ApplyFunctor;
-	typedef CloneableContainers::RemovableVector<ApplyFunctor> ContainerType;
-
-	template <typename T>
-	typename ContainerType::Identifier PushBack(T && item)
-	{
-		return enchantments_.PushBack(item.apply_functor);
-	}
-
-	template <typename T>
-	void Remove(T&& id)
-	{
-		return enchantments_.Remove(std::forward<T>(id));
-	}
-
-	void ApplyAll(State::Cards::Card & card)
-	{
-		enchantments_.IterateAll([&card](ApplyFunctor& functor) -> bool {
-			functor(card);
-			return true;
-		});
-	}
-
-private:
-	ContainerType enchantments_;
-};
--- dev/include/Enchantments/TieredEnchantments.h
+++ dev/include/Enchantments/TieredEnchantments.h
@@ -3,48 +3,54 @@
 #include "Enchantments/Enchantments.h"
 #include "Enchantments/Tiers.h"
 
-class TieredEnchantments
+namespace State
 {
-public:
-	using ContainerType = Enchantments::ContainerType;
-
-	template <typename T>
-	typename ContainerType::Identifier PushBack(T && item)
+	namespace Cards
 	{
-		using EnchantmentType = std::decay_t<T>;
-		return GetEnchantments<EnchantmentType::tier>().PushBack(std::forward<T>(item));
+		class TieredEnchantments
+		{
+		public:
+			using ContainerType = Enchantments::ContainerType;
+
+			template <typename T>
+			typename ContainerType::Identifier PushBack(T && item)
+			{
+				using EnchantmentType = std::decay_t<T>;
+				return GetEnchantments<EnchantmentType::tier>().PushBack(std::forward<T>(item));
+			}
+
+			template <typename EnchantmentType, typename T>
+			void Remove(T&& id)
+			{
+				return GetEnchantments<EnchantmentType::tier>().Remove(std::forward<T>(id));
+			}
+
+			void ApplyAll(Card & card)
+			{
+				tier1_.ApplyAll(card);
+				tier2_.ApplyAll(card);
+				tier3_.ApplyAll(card);
+				aura_.ApplyAll(card);
+			}
+
+		private:
+			template <int Tier> Enchantments & GetEnchantments();
+			template <int Tier> const Enchantments & GetEnchantments() const;
+
+		private:
+			Enchantments tier1_;
+			Enchantments tier2_;
+			Enchantments tier3_;
+			Enchantments aura_;
+		};
+
+		template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier1>() { return tier1_; }
+		template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier1>() const { return tier1_; }
+		template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier2>() { return tier2_; }
+		template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier2>() const { return tier2_; }
+		template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier3>() { return tier3_; }
+		template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier3>() const { return tier3_; }
+		template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentAura>() { return aura_; }
+		template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentAura>() const { return aura_; }
 	}
-
-	template <typename EnchantmentType, typename T>
-	void Remove(T&& id)
-	{
-		return GetEnchantments<EnchantmentType::tier>().Remove(std::forward<T>(id));
-	}
-
-	void ApplyAll(State::Cards::Card & card)
-	{
-		tier1_.ApplyAll(card);
-		tier2_.ApplyAll(card);
-		tier3_.ApplyAll(card);
-		aura_.ApplyAll(card);
-	}
-
-private:
-	template <int Tier> Enchantments & GetEnchantments();
-	template <int Tier> const Enchantments & GetEnchantments() const;
-
-private:
-	Enchantments tier1_;
-	Enchantments tier2_;
-	Enchantments tier3_;
-	Enchantments aura_;
-};
-
-template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier1>() { return tier1_; }
-template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier1>() const { return tier1_; }
-template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier2>() { return tier2_; }
-template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier2>() const { return tier2_; }
-template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier3>() { return tier3_; }
-template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentTier3>() const { return tier3_; }
-template <> Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentAura>() { return aura_; }
-template <> const Enchantments & TieredEnchantments::GetEnchantments<kEnchantmentAura>() const { return aura_; }
+}
