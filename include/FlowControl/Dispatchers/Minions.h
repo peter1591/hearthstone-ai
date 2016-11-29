6ea42622c22803521a611c6aafa5ffa6d3f3cdbd
Peter Shih <petershih@synology.com>
2016-11-29 10:44:33 +0800

    Use macro to define invoker in static dispatcher

--- include/FlowControl/Dispatchers/Minions.h
+++ include/FlowControl/Dispatchers/Minions.h
@@ -8,46 +8,41 @@ namespace FlowControl
 	{
 		namespace impl
 		{
-			template <typename T>
-			class BattleCryInvoker
-			{
-			private:
-				typedef long HasMethodType;
-				typedef char NoMethodType;
-
-				template <typename U, typename V = decltype(&T::BattleCry)>
-				struct SFINAE { using type = HasMethodType; };
-
-				template <typename U> static typename SFINAE<T>::type MethodExists(void*);
-				template <typename U> static NoMethodType MethodExists(...);
-
-			public:
-				template <typename... Args>
-				static void Invoke(Args&&... args)
-				{
-					using TestResult = decltype(MethodExists<T>(nullptr));
-					return InvokeInternal<TestResult>(nullptr, std::forward<Args>(args)...);
-				}
-
-			private:
-				template <typename TestResult, typename... Args>
-				static void InvokeInternal(std::enable_if_t<sizeof(TestResult) == sizeof(HasMethodType)>*, Args&&... args)
-				{
-					return T::BattleCry(std::forward<Args>(args)...);
-				}
-
-				template <typename TestResult, typename... Args>
-				static void InvokeInternal(std::enable_if_t<sizeof(TestResult) == sizeof(NoMethodType)>*, Args&&... args)
-				{
-					return;
-				}
+#define CREATE_INVOKER(METHOD_NAME) \
+			template <typename T> class METHOD_NAME ## Invoker \
+			{ \
+			private: \
+				typedef long HasMethodType; \
+				typedef char NoMethodType; \
+				template <typename U, typename V = decltype(&T::METHOD_NAME)> struct SFINAE { using type = HasMethodType; }; \
+				template <typename U> static typename SFINAE<T>::type MethodExists(void*); \
+				template <typename U> static NoMethodType MethodExists(...); \
+\
+			public: \
+				template <typename... Args> static void Invoke(Args&&... args) { \
+					using TestResult = decltype(MethodExists<T>(nullptr)); \
+					return InvokeInternal<TestResult>(nullptr, std::forward<Args>(args)...); \
+				} \
+\
+			private: \
+				template <typename TestResult, typename... Args> \
+				static void InvokeInternal(std::enable_if_t<sizeof(TestResult) == sizeof(HasMethodType)>*, Args&&... args) \
+				{ \
+					return T::METHOD_NAME(std::forward<Args>(args)...); \
+				} \
+\
+				template <typename TestResult, typename... Args> \
+				static void InvokeInternal(std::enable_if_t<sizeof(TestResult) == sizeof(NoMethodType)>*, Args&&... args) \
+				{ \
+					return; \
+				} \
 			};
 
-			class DefaultInvoked
-			{
-			public:
-				template <typename... Args> static void BattleCry(Args&&... args) {}
-			};
+			CREATE_INVOKER(BattleCry);
+
+#undef CREATE_INVOKER
+
+			class DefaultInvoked { };
 		}
 
 		class Minions
