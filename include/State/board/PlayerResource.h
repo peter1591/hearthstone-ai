17115b7915e06a1ba2685477764972d0ee294fe4
Peter Shih <petershih@synology.com>
2016-12-01 18:32:17 +0800

    Implement more crystal interface

--- include/State/board/PlayerResource.h
+++ include/State/board/PlayerResource.h
@@ -9,12 +9,13 @@ namespace state
 		public:
 			void SetTotal(int total) { total_ = total; }
 			int GetTotal() const { return total_; }
+			void IncreaseTotal(int amount = 1) { total_ += amount; }
 
 			void SetCurrent(int current) { current_ = current; }
 			int GetCurrent() const { return current_; }
+			void IncreaseCurrent(int amount = 1) { current_ += amount; }
 
 			void Refill() { current_ = total_; }
-			void IncreaseTotal(int amount = 1) { total_ += amount; }
 			void Cost(int amount) { current_ -= amount; }
 
 		private:
