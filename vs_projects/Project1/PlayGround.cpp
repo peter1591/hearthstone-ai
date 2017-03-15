431a7888647d967c0592bc60ceafedce177428b9
Peter Shih <petershih@synology.com>
2017-03-15 13:39:54 +0800

    modify playground

--- vs_projects/Project1/PlayGround.cpp
+++ vs_projects/Project1/PlayGround.cpp
@@ -1,50 +1,25 @@
 #include <iostream>
 #include <utility>
 
+#include "Utils/StaticInvokables.h"
+
 template <int v>
 struct StaticInvokable {
 	template <typename... Args>
-	static void Invoke(Args&...) {
+	static void Invoke(Args&&...) {
 		std::cout << "invoked: " << v << std::endl;
 	}
 };
 
-struct NullInvokable {
-	template <typename... Args> static void Invoke(Args&&...) {}
-};
-
-template <typename T, typename U>
-struct CombineStaticInvokable {
-	template <typename... Args>
-	static void Invoke(Args&... args) {
-		T::Invoke(args...);
-		U::Invoke(args...);
-	}
-};
-
-template <typename T = NullInvokable>
-class InvokableChain
-{
-public:
-	template <typename U>
-	struct Combine {
-		using type = InvokableChain<CombineStaticInvokable<T, U>>;
-	};
-
-	template <typename U>
-	using Combined = typename Combine<U>::type;
-
-	template <typename... Args>
-	static void Invoke(Args&&... args) { return T::Invoke(std::forward<Args>(args)...); }
-};
-
 int main(void)
 {
+	using Utils::StaticInvokableChain;
+
 	typedef StaticInvokable<1> T1;
 	typedef StaticInvokable<2> T2;
 
-	InvokableChain<>::Combined<T1>::Combined<T2>::Invoke();
-	//InvokableChain<>::Combine<T1>::Combine<T2>::Invoke();
+	void (*func_ptr)(void) = StaticInvokableChain<T1>::CombineType<T2>::Invoke;
+	(*func_ptr)();
 
 	return 0;
 }
