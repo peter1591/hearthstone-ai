c9af60bc60391302dbb43065b657e995b634c355
Peter Shih <petershih@synology.com>
2017-03-17 19:39:35 +0800

    Implement divine shield framework

--- include/FlowControl/Manipulators/Helpers/DamageHelper-impl.h
+++ include/FlowControl/Manipulators/Helpers/DamageHelper-impl.h
@@ -17,16 +17,21 @@ namespace FlowControl
 				int new_amount = amount; // TODO
 				if (new_amount <= 0) return;
 
-				DoDamage(state, flow_context, new_target, state.GetCard(new_target), new_amount);
+				state::Cards::Card const& new_target_card = state.GetCard(new_target);
+				if (new_target_card.GetRawData().enchanted_states.shielded) {
+					Manipulate(state, flow_context).Character(new_target).Shield(false);
+					return;
+				}
+
+				DoDamage(state, flow_context, new_target, new_target_card, new_amount);
 			}
 
 			inline void DamageHelper::DoDamage(state::State & state, FlowControl::FlowContext & flow_context, state::CardRef card_ref, state::Cards::Card const& card, int amount)
 			{
-				auto const& target_card = state.GetCardsManager().Get(card_ref);
 				state.TriggerEvent<state::Events::EventTypes::OnTakeDamage>(card_ref,
-					state::Events::EventTypes::OnTakeDamage::Context{ state, flow_context, target_card, amount });
+					state::Events::EventTypes::OnTakeDamage::Context{ state, flow_context, card, amount });
 				state.TriggerCategorizedEvent<state::Events::EventTypes::OnTakeDamage>(card_ref,
-					state::Events::EventTypes::OnTakeDamage::Context{ state, flow_context, target_card, amount });
+					state::Events::EventTypes::OnTakeDamage::Context{ state, flow_context, card, amount });
 
 				Manipulate(state, flow_context).Card(card_ref).Internal_SetDamage().TakeDamage(amount);
 				flow_context.AddDeadEntryHint(state, card_ref);
