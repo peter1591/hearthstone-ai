cc739a84b4640ecc8e40c98ad6b00dce43854de4
Peter Shih <petershih@synology.com>
2017-03-22 15:18:05 +0800

    state::State manages just_played flag

--- include/FlowControl/Manipulators/MinionManipulator.h
+++ include/FlowControl/Manipulators/MinionManipulator.h
@@ -19,7 +19,6 @@ namespace FlowControl
 
 			void AfterSummoned()
 			{
-				card_.SetJustPlayedFlag(true);
 			}
 
 			void TurnStart()
--- include/state/detail/InvokeCallback.h
+++ include/state/detail/InvokeCallback.h
@@ -4,16 +4,17 @@ namespace state {
 	namespace detail {
 		template <CardType CardType, CardZone CardZone>
 		struct InvokeCallback {
-			static void Added(state::State & state, state::CardRef card_ref, state::Cards::Card const& card) {}
-			static void Removed(state::State & state, state::CardRef card_ref, state::Cards::Card const& card) {}
+			static void Added(state::State & state, state::CardRef card_ref, state::Cards::Card & card) {}
+			static void Removed(state::State & state, state::CardRef card_ref, state::Cards::Card & card) {}
 		};
 
 		template <CardType CardType>
 		struct InvokeCallback<CardType, kCardZonePlay> {
-			static void Added(state::State & state, state::CardRef card_ref, state::Cards::Card const& card) {
+			static void Added(state::State & state, state::CardRef card_ref, state::Cards::Card & card) {
+				card.SetJustPlayedFlag(true);
 				card.GetRawData().added_to_play_zone(state::Cards::ZoneChangedContext{ state, card_ref, card });
 			}
-			static void Removed(state::State & state, state::CardRef card_ref, state::Cards::Card const& card) {}
+			static void Removed(state::State & state, state::CardRef card_ref, state::Cards::Card & card) {}
 		};
 
 		// TODO: added to deck zone callback
