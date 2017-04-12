7bccc826c1fba42464d3d1405808cefdef6eb528
Peter Shih <petershih@synology.com>
2017-04-12 10:20:49 +0800

    Refine static event manager

--- include/Utils/StaticEventTriggerer.h
+++ include/Utils/StaticEventTriggerer.h
@@ -6,27 +6,7 @@ namespace Utils
 {
 	namespace StaticEventTriggererImpl
 	{
-		template <class FirstType, class... RestTypes>
-		class Invoker2
-		{
-		public:
-			template <class... Args>
-			static void Trigger(Args&&... args)
-			{
-				FirstType::Trigger(std::forward<Args>(args)...);
-				Invoker<RestTypes...>::Trigger(std::forward<Args>(args)...);
-			}
-		};
-
-		template <class... Triggers> class Invoker
-		{
-		public:
-			template <class... Args>
-			static void Trigger(Args&&... args)
-			{
-				Invoker2<Triggers...>::Trigger(std::forward<Args>(args)...);
-			}
-		};
+		template <class... Triggers> class Invoker;
 
 		template <> class Invoker<>
 		{
@@ -34,6 +14,29 @@ namespace Utils
 			template <class... Args>
 			static void Trigger(Args&&... args)
 			{
+			}
+		};
+
+		template <class Invokee>
+		class Invoker<Invokee>
+		{
+		public:
+			template <class... Args>
+			static void Trigger(Args&&... args)
+			{
+				Invokee::Trigger(std::forward<Args>(args)...);
+			}
+		};
+
+		template <class FirstType, class... RestTypes>
+		class Invoker <FirstType, RestTypes...>
+		{
+		public:
+			template <class... Args>
+			static void Trigger(Args&&... args)
+			{
+				Invoker<FirstType>::Trigger(std::forward<Args>(args)...);
+				Invoker<RestTypes...>::Trigger(std::forward<Args>(args)...);
 			}
 		};
 	}
