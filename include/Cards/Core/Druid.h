2139bc6efd199b38400b53cac3b21cd9e0b9ab52
Peter Shih <petershih@synology.com>
2017-04-18 18:07:33 +0800

    Implement card

--- include/Cards/Core/Druid.h
+++ include/Cards/Core/Druid.h
@@ -108,8 +108,25 @@ namespace Cards
 			});
 		}
 	};
+
+	struct Card_EX1_173 : public SpellCardBase<Card_EX1_173> {
+		Card_EX1_173() {
+			onplay_handler.SetSpecifyTargetCallback([](Contexts::SpecifiedTargetGetter const& context) {
+				*context.allow_no_target = false;
+				return TargetsGenerator(context.player_).SpellTargetable().GetInfo();
+			});
+			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
+				int spell_damage = context.manipulate_.Board().GetSpellDamage(context.player_);
+				state::CardRef target = context.GetTarget();
+				if (!target.IsValid()) return;
+				context.manipulate_.OnBoardCharacter(target).Damage(context.card_ref_, spell_damage + 5);
+				context.manipulate_.Hero(context.player_).DrawCard();
+			});
+		}
+	};
 }
 
+REGISTER_CARD(EX1_173)
 REGISTER_CARD(CS2_012)
 REGISTER_CARD(CS2_011)
 REGISTER_CARD(CS2_007)
