ed59bc23ca4cc60b5ba730c389500f7f08dc1e30
Peter Shih <petershih@synology.com>
2017-04-18 18:21:24 +0800

    Implement card

--- include/Cards/Core/Hunter.h
+++ include/Cards/Core/Hunter.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// http://www.hearthpwn.com/cards?filter-premium=1&filter-set=2&filter-class=8&sort=-cost&display=1
+// Last-finished card: Hunter's Mark
+
 namespace Cards
 {
 	struct Card_DS1_185 : public SpellCardBase<Card_DS1_185> {
@@ -17,7 +20,21 @@ namespace Cards
 		}
 	};
 
-	struct Card_CS2_084e : public 
+	struct Card_CS2_084e : public Enchantment<Card_CS2_084e, SetMaxHP<1>> {};
+	struct Card_CS2_084 : public SpellCardBase<Card_CS2_084> {
+		Card_CS2_084() {
+			onplay_handler.SetSpecifyTargetCallback([](Contexts::SpecifiedTargetGetter const& context) {
+				*context.allow_no_target = false;
+				return TargetsGenerator(context.player_).Minion().SpellTargetable().GetInfo();
+			});
+			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
+				state::CardRef target = context.GetTarget();
+				if (!target.IsValid()) return;
+				context.manipulate_.OnBoardMinion(target).Enchant().Add<Card_CS2_084e>();
+			});
+		}
+	};
 }
 
+REGISTER_CARD(CS2_084)
 REGISTER_CARD(DS1_185)
