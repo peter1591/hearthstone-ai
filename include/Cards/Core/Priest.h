58511de1945059e0bc71cb6b8fa29ee8f03c5dbf
Peter Shih <petershih@synology.com>
2017-04-22 01:05:10 +0800

    Implement card

--- include/Cards/Core/Priest.h
+++ include/Cards/Core/Priest.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// http://www.hearthpwn.com/cards?filter-set=2&filter-class=64&sort=-cost&display=1
+
 namespace Cards
 {
 	struct Card_CS1h_001 : public HeroPowerCardBase<Card_CS1h_001> {
@@ -15,6 +17,22 @@ namespace Cards
 			});
 		}
 	};
+
+	struct Card_CS1_130 : public SpellCardBase<Card_CS1_130> {
+		Card_CS1_130() {
+			onplay_handler.SetSpecifyTargetCallback([](Contexts::SpecifiedTargetGetter const& context) {
+				*context.targets_ = TargetsGenerator(context.player_).SpellTargetable().GetInfo();
+				return true;
+			});
+			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
+				int spell_damage = context.manipulate_.Board().GetSpellDamage(context.player_);
+				state::CardRef target = context.GetTarget();
+				if (!target.IsValid()) return;
+				context.manipulate_.OnBoardCharacter(target).Damage(context.card_ref_, 2 + spell_damage);
+			});
+		}
+	};
 }
 
-REGISTER_CARD(CS1h_001);
+REGISTER_CARD(CS1_130)
+REGISTER_CARD(CS1h_001)
