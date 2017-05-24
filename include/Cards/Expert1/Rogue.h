6b85cf6eaeb9055b4d6815fe67402d86d79f492e
Peter Shih <petershih@synology.com>
2017-05-24 23:52:51 +0800

    Implement card

---
+++ include/Cards/Expert1/Rogue.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// http://www.hearthpwn.com/cards?filter-set=3&filter-class=128&sort=-cost&display=1
+// Last finished card: (none)
+
+namespace Cards
+{
+	struct Card_EX1_145 : SpellCardBase<Card_EX1_145> {
+		Card_EX1_145() {
+			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
+				int turn = context.manipulate_.Board().GetTurn();
+				context.manipulate_.AddEvent<state::Events::EventTypes::GetPlayCardCost>(
+					[turn](state::Events::EventTypes::GetPlayCardCost::Context context) mutable
+				{
+					if (context.manipulate_.Board().GetTurn() != turn) return false;
+
+					if (context.manipulate_.GetCard(context.card_ref_).GetCardType() != state::kCardTypeSpell) return true;
+					*context.cost_ -= 3;
+					return false;
+				});
+			});
+		}
+	};
+}
+
+REGISTER_CARD(EX1_145)
