fc175182756b2d03ea83dd6f8de33dcea6788c31
Shen-En Shih <petershih@google.com>
2017-05-28 19:07:36 -0700

    Implement cards

--- include/Cards/Expert1/Warlock.h
+++ include/Cards/Expert1/Warlock.h
@@ -1,7 +1,65 @@
 #pragma once
 
+// http://www.hearthpwn.com/cards?filter-set=3&filter-class=512&sort=-cost&display=1
+// Last Finished Card: (none)
+
 namespace Cards
 {
+	struct Card_CS2_059o : Enchantment<Card_CS2_059o, MaxHP<1>> {};
+	struct Card_CS2_059 : MinionCardBase<Card_CS2_059, Stealth> {
+		static bool HandleEvent(state::CardRef self, state::Events::EventTypes::OnTurnEnd::Context context) {
+			state::PlayerIdentifier owner = context.manipulate_.GetCard(self).GetPlayerIdentifier();
+			if (owner != context.manipulate_.Board().GetCurrentPlayerId()) return true;
+
+			int pos = context.manipulate_.GetCard(self).GetZonePosition();
+			int count = (int)context.manipulate_.Board().Player(owner).minions_.Size();
+
+			assert(count > 0);
+			
+			if (count == 1) return true;
+
+			int pick_pos = context.manipulate_.GetRandom().Get(count - 1);
+			if (pick_pos >= pos) ++pick_pos;
+
+			state::CardRef pick_ref = context.manipulate_.Board().Player(owner).minions_.Get(pick_pos);
+			context.manipulate_.OnBoardMinion(pick_ref).Enchant().Add<Card_CS2_059o>();
+		}
+		Card_CS2_059() {
+			RegisterEvent<MinionInPlayZone, NonCategorized_SelfInLambdaCapture,
+				state::Events::EventTypes::OnTurnEnd>();
+		}
+	};
+
+	struct Card_EX1_319 : MinionCardBase<Card_EX1_319> {
+		static void Battlecry(Contexts::OnPlay const& context) {
+			context.manipulate_.Hero(context.player_).Damage(context.card_ref_, 3);
+		}
+	};
+
+	struct Card_EX1_596e : Enchantment<Card_EX1_596e, Attack<2>, MaxHP<2>> {};
+	struct Card_EX1_596 : SpellCardBase<Card_EX1_596> {
+		Card_EX1_596() {
+			onplay_handler.SetSpecifyTargetCallback([](Contexts::SpecifiedTargetGetter & context) {
+				context.SetRequiredTargets(context.player_).Minion();
+				return true;
+			});
+			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
+				bool friendly_demon = false;
+				if (context.manipulate_.GetCard(context.card_ref_).GetPlayerIdentifier() == context.player_) {
+					if (context.manipulate_.GetCard(context.card_ref_).GetRace() == state::kCardRaceDemon) {
+						friendly_demon = true;
+					}
+				}
+				if (friendly_demon) {
+					context.manipulate_.OnBoardMinion(context.card_ref_).Enchant().Add<Card_EX1_596e>();
+				}
+				else {
+					context.manipulate_.OnBoardMinion(context.card_ref_).Damage(context.card_ref_, 2);
+				}
+			});
+		}
+	};
+
 	struct Card_EX1_312 : SpellCardBase<Card_EX1_312> {
 		Card_EX1_312() {
 			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
@@ -16,3 +74,7 @@ namespace Cards
 }
 
 REGISTER_CARD(EX1_312)
+
+REGISTER_CARD(EX1_596)
+REGISTER_CARD(EX1_319)
+REGISTER_CARD(CS2_059)
