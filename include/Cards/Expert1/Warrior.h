e72e156e92d92f2d19e42125c619d9b1f79e8558
Shen-En Shih <petershih@google.com>
2017-05-31 17:33:37 +0800

    Implement card

--- include/Cards/Expert1/Warrior.h
+++ include/Cards/Expert1/Warrior.h
@@ -66,8 +66,40 @@ namespace Cards
 			});
 		}
 	};
+
+	struct Card_NEW1_036 : SpellCardBase<Card_NEW1_036> {
+		Card_NEW1_036() {
+			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
+				state::PlayerIdentifier player = context.player_;
+				context.manipulate_.AddEvent<state::Events::EventTypes::OnTakeDamage>(
+					[&](state::Events::EventTypes::OnTakeDamage::Context context) {
+					auto const& card = context.manipulate_.GetCard(context.card_ref_);
+					if (card.GetCardType() != state::kCardTypeMinion) return true;
+					if (card.GetPlayerIdentifier() != player) return true;
+					int max_damage = card.GetHP() - 1;
+					if (*context.damage_ > max_damage) *context.damage_ = max_damage;
+					return true;
+				});
+			});
+		}
+	};
+
+	struct Card_CS2_104e : Enchantment<Card_CS2_104e, Attack<3>, MaxHP<3>> {};
+	struct Card_CS2_104 : SpellCardBase<Card_CS2_104> {
+		Card_CS2_104() {
+			onplay_handler.SetSpecifyTargetCallback([](Contexts::SpecifiedTargetGetter & context) {
+				context.SetRequiredTargets(context.player_).Minion().Damaged(); // TODO: only spell targetable
+				return true;
+			});
+			onplay_handler.SetOnPlayCallback([](FlowControl::onplay::context::OnPlay const& context) {
+				context.manipulate_.OnBoardMinion(context.GetTarget()).Enchant().Add<Card_CS2_104e>();
+			});
+		}
+	};
 }
 
+REGISTER_CARD(CS2_104)
+REGISTER_CARD(NEW1_036)
 REGISTER_CARD(EX1_392)
 REGISTER_CARD(EX1_409)
 REGISTER_CARD(EX1_410)
