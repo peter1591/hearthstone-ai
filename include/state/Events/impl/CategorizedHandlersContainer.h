845ebc44c08629872d79d5c91470f0314076fb2c
Shen-En Shih <petershih@google.com>
2017-06-01 16:13:03 +0800

    Use hash table for categorized handlers

--- include/state/Events/impl/CategorizedHandlersContainer.h
+++ include/state/Events/impl/CategorizedHandlersContainer.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <utility>
-#include <vector>
+#include <unordered_map>
 #include "state/Events/impl/HandlersContainer.h"
 #include "state/Types.h"
 
@@ -22,22 +22,17 @@ namespace state
 				template <typename HandlerType_>
 				void PushBack(CardRef card_ref, HandlerType_&& handler)
 				{
-					if (card_ref.id >= categories_.size()) {
-						categories_.resize(card_ref.id + 1);
-					}
 					categories_[card_ref.id].PushBack(std::forward<HandlerType_>(handler));
 				}
 
 				template <typename... Args>
 				void TriggerAll(CardRef card_ref, Args&&... args)
 				{
-					if (card_ref.id >= categories_.size()) return;
 					categories_[card_ref.id].TriggerAll(card_ref, std::forward<Args>(args)...);
 				}
 
 			private:
-				// TODO: do not use vector, since only few of cards will be bound with event handlers
-				std::vector<HandlersContainer<HandlerType>> categories_;
+				std::unordered_map<state::CardRef::IdentifierType, HandlersContainer<HandlerType>> categories_;
 			};
 		}
 	}
