0a4a1d45a4e6bcabc62171b603f363b54a4c3987
Shen-En Shih <petershih@google.com>
2017-06-05 01:43:13 +0800

    Add MCTS stages interface

--- include/MCTS/MCTS.h
+++ include/MCTS/MCTS.h
@@ -1,9 +1,42 @@
 #pragma once
 
+#include "MCTS/Board.h"
+
 namespace mcts
 {
 	class MCTS
 	{
 	public:
+		template <typename StartBoardGetter>
+		void Iterate(StartBoardGetter&& start_board_getter) {
+			StartEpisode(std::forward<StartBoardGetter>(start_board_getter));
+			Select();
+			Expand();
+			Simulate();
+			BackPropagate();
+		}
+
+	private:
+		template <typename StartBoardGetter>
+		void StartEpisode(StartBoardGetter start_board_getter) {
+			board_ = start_board_getter.GetBoard();
+		}
+
+		void Select() {
+
+		}
+
+		void Expand() {
+
+		}
+
+		void Simluate() {
+
+		}
+
+		void BackPropagate();
+
+	private:
+		Board board_;
 	};
 }
