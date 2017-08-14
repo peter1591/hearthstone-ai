956cde5bca4b87ba984708afab670756bae92e50
Shen-En Shih <petershih@google.com>
2017-08-14 13:26:41 +0800

    Implement CoW on secrets

--- include/state/board/Secrets.h
+++ include/state/board/Secrets.h
@@ -13,45 +13,73 @@ namespace state
 	{
 		class Secrets
 		{
+		private:
+			using ContainerType = std::unordered_map<int, CardRef>;
+
 		public:
-			Secrets() : secrets_() {}
+			Secrets() : base_(nullptr), secrets_() {}
+
+			Secrets(Secrets const& rhs) : base_(rhs.base_), secrets_(rhs.secrets_) {}
+			Secrets & operator=(Secrets const& rhs) {
+				base_ = rhs.base_;
+				secrets_ = rhs.secrets_;
+				return *this;
+			}
 
 			void FillWithBase(Secrets const& base) {
-				secrets_ = base.secrets_; // TODO: copy on write
+				assert(base.base_ == nullptr);
+				base_ = &base.secrets_;
 			}
 
 			bool Exists(::Cards::CardId card_id) const
 			{
-				return secrets_.find((int)card_id) != secrets_.end();
+				ContainerType const& container = GetContainer();
+				return container.find((int)card_id) != container.end();
 			}
 
 			template <typename Functor>
 			void ForEach(Functor&& functor) const {
-				for (auto const& kv : secrets_) {
+				ContainerType const& container = GetContainer();
+				for (auto const& kv : container) {
 					functor(kv.second);
 				}
 			}
 
-			bool Empty() const { return secrets_.empty(); }
+			bool Empty() const { return GetContainer().empty(); }
 
 			void Add(::Cards::CardId card_id, CardRef card)
 			{
 				if (Exists(card_id)) throw std::runtime_error("Secret already exists");
-				secrets_.insert(std::make_pair((int)card_id, card));
+				GetContainerForWrite().insert(std::make_pair((int)card_id, card));
 			}
 
 			void Remove(::Cards::CardId card_id)
 			{
-				secrets_.erase(card_id);
+				GetContainerForWrite().erase(card_id);
 			}
 
 			void Clear()
 			{
-				secrets_.clear();
+				GetContainerForWrite().clear();
 			}
 
 		private:
-			std::unordered_map<int, CardRef> secrets_;
+			ContainerType & GetContainerForWrite() {
+				if (base_) {
+					secrets_ = *base_;
+					base_ = nullptr;
+				}
+				return secrets_;
+			}
+
+			ContainerType const& GetContainer() const {
+				if (base_) return *base_;
+				else return secrets_;
+			}
+
+		private:
+			ContainerType const* base_;
+			ContainerType secrets_;
 		};
 	}
 }
