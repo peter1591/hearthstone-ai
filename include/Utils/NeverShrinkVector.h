03dbe667220b795fe3002efb6ba06bfb0e0e7563
Shen-En Shih <petershih@google.com>
2017-08-16 01:26:21 +0800

    Implement never-shrink vector
    Wrap over std::vector

---
+++ include/Utils/NeverShrinkVector.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <vector>
+
+namespace Utils
+{
+	template <class ItemType>
+	class NeverShrinkVector {
+	public:
+		NeverShrinkVector() : size_(), container_() {}
+
+		auto begin() {
+			return container_.begin();
+		}
+		auto end() {
+			return container_.begin() + size_;
+		}
+
+		auto const& operator[](size_t idx) const {
+			assert(idx < size_);
+			return container_[idx];
+		}
+		auto & operator[](size_t idx) {
+			assert(idx < size_);
+			return container_[idx];
+		}
+
+		void clear() { size_ = 0; }
+		size_t size() const { return size_; }
+
+		void resize(size_t size) {
+			size_t fill_to = std::min(size, container_.size());
+			for (size_t idx = 0; idx < fill_to; ++idx) {
+				container_[idx] = ItemType();
+			}
+
+			if (size > fill_to) {
+				container_.resize(size);
+			}
+			size_ = size;
+		}
+
+		template <class Arg>
+		void push_back(Arg&& arg) {
+			if (size_ < container_.size()) {
+				container_[size_] = arg;
+				++size_;
+			}
+			else {
+				assert(size_ == container_.size());
+				container_.push_back(std::forward<Arg>(arg));
+				++size_;
+			}
+		}
+
+	private:
+		size_t size_;
+		std::vector<ItemType> container_;
+	};
+}
