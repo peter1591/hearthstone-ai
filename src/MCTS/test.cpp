88fedc00d11ffaf4a66f2be649b486696bcc9a1e
Shen-En Shih <petershih@google.com>
2017-09-29 00:32:15 +0800

    mcts: Prepare to refactor out interactive shell

--- src/MCTS/test.cpp
+++ src/MCTS/test.cpp
@@ -17,180 +17,172 @@ static void Initialize()
 	std::cout << " Done." << std::endl;
 }
 
-class Handler
+class InteractiveShellHandler
 {
 public:
-	Handler() : controller_(), node_(nullptr) {}
+	InteractiveShellHandler(int tree_samples, std::mt19937 & rand) :
+		controller_(tree_samples, rand), node_(nullptr), threads_(1)
+	{}
 
-	Handler(Handler const&) = delete;
-	Handler & operator=(Handler const&) = delete;
+	InteractiveShellHandler(InteractiveShellHandler const&) = delete;
+	InteractiveShellHandler & operator=(InteractiveShellHandler const&) = delete;
 
-	void Start()
+	void DoCommand(std::istream & is, std::ostream & s)
 	{
-		auto start_board_getter = [](int seed) -> state::State {
-			return TestStateBuilder().GetState(seed);
-		};
+		std::string cmd;
+		is >> cmd;
+		if (cmd == "h" || cmd == "help") {
+			s << "Commands: " << std::endl
+				<< "h or help: show this message" << std::endl
+				<< "threads: set number of threads" << std::endl
+				<< "s or start (secs): to run for a specified seconds" << std::endl
+				<< "root (1 or 2): set node to root node of player 1 or 2" << std::endl
+				<< "info: show info for selected node" << std::endl
+				<< "node (addr): set node to specified address." << std::endl;
+		}
+		else if (cmd == "threads") {
+			is >> threads_;
+			s << "Set thread count to " << threads_ << std::endl;
+		}
+		else if (cmd == "s" || cmd == "start") {
+			auto start_board_getter = [](int seed) -> state::State {
+				return TestStateBuilder().GetState(seed);
+			};
 
-		int threads = 1;
-		while (std::cin) {
-			std::string cmd;
-			std::cout << "Command: ";
-			std::cin >> cmd;
-
-			if (cmd == "h" || cmd == "help") {
-				std::cout << "Commands: " << std::endl
-					<< "h or help: show this message" << std::endl
-					<< "threads: set number of threads" << std::endl
-					<< "s or start (secs): to run for a specified seconds" << std::endl
-					<< "root (1 or 2): set node to root node of player 1 or 2" << std::endl
-					<< "info: show info for selected node" << std::endl
-					<< "node (addr): set node to specified address." << std::endl
-					<< "q or quit: quit" << std::endl;
-			}
-			else if (cmd == "threads") {
-				std::cin >> threads;
-				std::cout << "Set thread count to " << threads << std::endl;
-			}
-			else if (cmd == "s" || cmd == "start") {
-				int secs = 0;
-				std::cin >> secs;
+			int secs = 0;
+			is >> secs;
 				
-				auto seed = std::random_device()();
+			auto seed = std::random_device()();
 
-				std::cout << "Running for " << secs << " seconds with " << threads << " threads "
-					<< "(seed = " << seed << ")" << std::endl;
+			s << "Running for " << secs << " seconds with " << threads_ << " threads "
+				<< "(seed = " << seed << ")" << std::endl;
 
-				auto start = std::chrono::steady_clock::now();
-				std::chrono::steady_clock::time_point run_until =
-					std::chrono::steady_clock::now() +
-					std::chrono::seconds(secs);
+			auto start = std::chrono::steady_clock::now();
+			std::chrono::steady_clock::time_point run_until =
+				std::chrono::steady_clock::now() +
+				std::chrono::seconds(secs);
 
-				long long last_show_rest_sec = -1;
-				auto continue_checker = [&]() {
-					auto now = std::chrono::steady_clock::now();
-					if (now > run_until) return false;
-
-					auto rest_sec = std::chrono::duration_cast<std::chrono::seconds>(run_until - now).count();
-					if (rest_sec != last_show_rest_sec) {
-						std::cout << "Rest seconds: " << rest_sec << std::endl;
-						last_show_rest_sec = rest_sec;
-					}
-					return true;
-				};
-
-				auto start_i = controller_.GetStatistic().GetSuccededIterates();
-				controller_.Run(continue_checker, threads, seed, start_board_getter);
-				auto end_i = controller_.GetStatistic().GetSuccededIterates();
-
-				std::cout << std::endl;
-				std::cout << "Done iterations: " << (end_i - start_i) << std::endl;
-				std::cout << "====== Statistics =====" << std::endl;
-				controller_.GetStatistic().GetDebugMessage();
-
+			long long last_show_rest_sec = -1;
+			auto continue_checker = [&]() {
 				auto now = std::chrono::steady_clock::now();
-				auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
-				auto speed = (double)(end_i - start_i) / ms * 1000;
-				std::cout << "Iterations per second: " << speed << std::endl;
-				std::cout << std::endl;
-			}
-			else if (cmd == "root") {
-				DoRoot();
-			}
-			else if (cmd == "info") {
-				DoInfo();
-			}
-			else if (cmd == "node") {
-				uint64_t v = 0;
-				std::cin >> std::hex >> v >> std::dec;
-				node_ = (mcts::builder::TreeBuilder::TreeNode *)(v);
-				std::cout << "Node set to: " << node_ << std::endl;
-			}
-			else if (cmd == "q" || cmd == "quit") {
-				std::cout << "Good bye!" << std::endl;
-				break;
-			}
-			else {
-				std::cout << "Unknown command. Type 'h' or 'help' for usage help." << std::endl;
-			}
+				if (now > run_until) return false;
+
+				auto rest_sec = std::chrono::duration_cast<std::chrono::seconds>(run_until - now).count();
+				if (rest_sec != last_show_rest_sec) {
+					s << "Rest seconds: " << rest_sec << std::endl;
+					last_show_rest_sec = rest_sec;
+				}
+				return true;
+			};
+
+			auto start_i = controller_.GetStatistic().GetSuccededIterates();
+			controller_.Run(continue_checker, threads_, seed, start_board_getter);
+			auto end_i = controller_.GetStatistic().GetSuccededIterates();
+
+			s << std::endl;
+			s << "Done iterations: " << (end_i - start_i) << std::endl;
+			s << "====== Statistics =====" << std::endl;
+			controller_.GetStatistic().GetDebugMessage();
+
+			auto now = std::chrono::steady_clock::now();
+			auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+			auto speed = (double)(end_i - start_i) / ms * 1000;
+			s << "Iterations per second: " << speed << std::endl;
+			s << std::endl;
+		}
+		else if (cmd == "root") {
+			DoRoot(is, s);
+		}
+		else if (cmd == "info") {
+			DoInfo(s);
+		}
+		else if (cmd == "node") {
+			uint64_t v = 0;
+			is >> std::hex >> v >> std::dec;
+			node_ = (mcts::builder::TreeBuilder::TreeNode *)(v);
+			s << "Node set to: " << node_ << std::endl;
+		}
+		else {
+			s << "Unknown command. Type 'h' or 'help' for usage help." << std::endl;
 		}
 	}
 
 private:
-	void DoRoot()
+	void DoRoot(std::istream & is, std::ostream & s)
 	{
 		int v = 0;
-		std::cin >> v;
+		is >> v;
 
 		state::PlayerIdentifier side;
 		if (v == 1) side = state::PlayerIdentifier::First();
 		else if (v == 2) side = state::PlayerIdentifier::Second();
 		else {
-			std::cout << "invalid input" << std::endl;
+			s << "invalid input" << std::endl;
 			return;
 		}
 		node_ = controller_.GetRootNode(side);
-		std::cout << "Current node set to: " << node_ << std::endl;
+		s << "Current node set to: " << node_ << std::endl;
 	}
 
-	void DoInfo()
+	void DoInfo(std::ostream & s)
 	{
 		if (!node_) {
-			std::cout << "Should set node first." << std::endl;
+			s << "Should set node first." << std::endl;
 			return;
 		}
 
-		std::cout << "Action type: " << GetActionType(node_->GetActionType()) << std::endl;
+		s << "Action type: " << GetActionType(node_->GetActionType()) << std::endl;
 
 		if (node_->GetActionType() == mcts::ActionType::kMainAction) {
-			std::cout << "Playable hand cards:";
+			s << "Playable hand cards:";
 			auto * board_view = node_->GetAddon().consistency_checker.GetBoard();
 			node_->GetAddon().action_analyzer.ForEachPlayableCard([&](size_t idx) {
-				std::cout << " [" << idx << "] ";
+				s << " [" << idx << "] ";
 				if (board_view) {
-					std::cout << board_view->GetSelfHand()[idx].card_id;
+					s << board_view->GetSelfHand()[idx].card_id;
 				}
 				return true;
 			});
-			std::cout << std::endl;
+			s << std::endl;
 		}
 
-		std::cout << "Children:" << std::endl;
+		s << "Children:" << std::endl;
 		node_->ForEachChild([&](int choice, mcts::selection::ChildType const& child) {
 			auto const* edge_addon = node_->GetEdgeAddon(choice);
 
 			if (child.IsRedirectNode()) {
-				std::cout << "  " << choice << ": [REDIRECT]" << std::endl;
+				s << "  " << choice << ": [REDIRECT]" << std::endl;
 			}
 			else {
-				std::cout << "  " << choice << ": [NORMAL] "
+				s << "  " << choice << ": [NORMAL] "
 					<< child.GetNode() << std::endl;
 			}
 
 			if (node_->GetActionType() == mcts::ActionType::kMainAction) {
 				auto op = node_->GetAddon().action_analyzer.GetMainOpType(choice);
-				std::cout << "    Main Action Op: " << GetOpType(op) << std::endl;
+				s << "    Main Action Op: " << GetOpType(op) << std::endl;
 			}
 
 			if (edge_addon) {
-				std::cout << "    Chosen time: " << edge_addon->GetChosenTimes() << std::endl;
+				s << "    Chosen time: " << edge_addon->GetChosenTimes() << std::endl;
 
 				double credit_percentage = (double)edge_addon->GetCredit() / edge_addon->GetTotal() * 100;
-				std::cout << "    Credit: " << edge_addon->GetCredit() << " / " << edge_addon->GetTotal()
+				s << "    Credit: " << edge_addon->GetCredit() << " / " << edge_addon->GetTotal()
 					<< " (" << credit_percentage << "%)"
 					<< std::endl;
 			}
 			return true;
 		});
-		std::cout << "BoardNodeMap:" << std::endl;
+		s << "BoardNodeMap:" << std::endl;
 		node_->GetAddon().board_node_map.ForEach([&](mcts::board::BoardView board_view, mcts::builder::TreeBuilder::TreeNode* node) {
 			uint64_t total_chosen_time = 0;
 			node->ForEachChild([&](int choice, mcts::selection::ChildType const& child) {
 				total_chosen_time += child.GetEdgeAddon().GetChosenTimes();
 				return true;
 			});
-			std::cout << "  " << node << " Chosen time: " << total_chosen_time
+			s << "  " << node << " Chosen time: " << total_chosen_time
 				<< std::endl;
-			PrintBoardView(board_view, "    ");
+			PrintBoardView(board_view, "    ", s);
 			return true;
 		});
 	}
@@ -223,45 +215,45 @@ private:
 		}
 	}
 
-	void PrintBoardView(mcts::board::BoardView const& view, std::string const& line_prefix) {
-		std::cout << line_prefix << "Side: " << GetSideStr(view.GetSide())
+	void PrintBoardView(mcts::board::BoardView const& view, std::string const& line_prefix, std::ostream & s) {
+		s << line_prefix << "Side: " << GetSideStr(view.GetSide())
 			<< std::endl;
 
-		std::cout << line_prefix << "Self resource: "
+		s << line_prefix << "Self resource: "
 			<< view.GetSelfCrystal().current << " / " << view.GetSelfCrystal().total
 			<< std::endl;
 
-		std::cout << line_prefix << "Self Hero: "
+		s << line_prefix << "Self Hero: "
 			<< view.GetSelfHero().hp << " / " << view.GetSelfHero().max_hp
 			<< " armor: " << view.GetSelfHero().armor
 			<< std::endl;
 
-		std::cout << line_prefix << "Self hand cards: ";
+		s << line_prefix << "Self hand cards: ";
 		for (auto const& card : view.GetSelfHand()) {
-			std::cout << card.card_id << " ";
+			s << card.card_id << " ";
 		}
-		std::cout << std::endl;
+		s << std::endl;
 
-		std::cout << line_prefix << "Self Minions: ";
+		s << line_prefix << "Self Minions: ";
 		for (auto const& minion : view.GetSelfMinions()) {
-			std::cout << minion.hp << "/" << minion.max_hp << " ";
+			s << minion.hp << "/" << minion.max_hp << " ";
 		}
-		std::cout << std::endl;
+		s << std::endl;
 
-		std::cout << line_prefix << "Opponent resource: "
+		s << line_prefix << "Opponent resource: "
 			<< view.GetOpponentCrystal().current << " / " << view.GetOpponentCrystal().total
 			<< std::endl;
 
-		std::cout << line_prefix << "Opponent Hero: "
+		s << line_prefix << "Opponent Hero: "
 			<< view.GetOpponentHero().hp << " / " << view.GetOpponentHero().max_hp
 			<< " armor: " << view.GetOpponentHero().armor
 			<< std::endl;
 
-		std::cout << line_prefix << "Opponent Minions: ";
+		s << line_prefix << "Opponent Minions: ";
 		for (auto const& minion : view.GetOpponentMinions()) {
-			std::cout << minion.hp << "/" << minion.max_hp << " ";
+			s << minion.hp << "/" << minion.max_hp << " ";
 		}
-		std::cout << std::endl;
+		s << std::endl;
 	}
 
 	std::string GetSideStr(state::PlayerSide side) {
@@ -274,14 +266,31 @@ private:
 private:
 	ui::AIController controller_;
 	mcts::builder::TreeBuilder::TreeNode const* node_;
+	int threads_;
 };
 
 void TestAI()
 {
 	srand(0);
 
-	Handler handler;
-	handler.Start();
+	int tree_samples = 100;
+	std::mt19937 rand;
+
+	InteractiveShellHandler handler(tree_samples, rand);
+
+	while (std::cin) {
+		std::string cmdline;
+		std::cout << "Command: ";
+		std::getline(std::cin, cmdline);
+
+		if (cmdline == "q" || cmdline == "quit") {
+			std::cout << "Good bye!" << std::endl;
+			break;
+		}
+
+		std::istringstream iss(cmdline);
+		handler.DoCommand(iss, std::cout);
+	}
 }
 
 int main(int argc, char *argv[])
