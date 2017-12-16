a8f2e54490a30c64119e612d18d9de62657655bc
Shen-En Shih <petershih@google.com>
2017-12-17 02:03:33 +0800

    engine: commit missing main-op header file

---
+++ engine/include/FlowControl/MainOp.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cassert>
+#include <string>
+
+namespace FlowControl
+{
+	enum MainOpType {
+		kMainOpPlayCard,
+		kMainOpAttack,
+		kMainOpHeroPower,
+		kMainOpEndTurn,
+		kMainOpInvalid, kMainOpMax = kMainOpInvalid
+	};
+
+	inline std::string GetMainOpString(MainOpType op) {
+		switch (op) {
+		case MainOpType::kMainOpInvalid: return "kInvalid";
+		case MainOpType::kMainOpPlayCard: return "kPlayCard";
+		case MainOpType::kMainOpAttack: return "kAttack";
+		case MainOpType::kMainOpHeroPower: return "kHeroPower";
+		case MainOpType::kMainOpEndTurn: return "kEndTurn";
+		default:
+			assert(false);
+			return "Unknown!!!";
+		}
+	}
+}
