67c15bfb0f696aa327d329db1700e786a93590ef
Shen-En Shih <petershih@google.com>
2018-01-03 15:29:02 +0800

    agents: MCTS: refine selection policy implementation

--- agents/include/MCTS/policy/Selection.h
+++ agents/include/MCTS/policy/Selection.h
@@ -17,42 +17,28 @@ namespace mcts
 
 			class ChoiceIterator {
 			public:
+				struct Item {
+					int choice;
+					mcts::selection::EdgeAddon const* edge_addon;
+				};
+
 				ChoiceIterator(engine::ActionChoices & choices, mcts::selection::ChildNodeMap const& children) :
-					choices_(choices), children_(children),
-					current_choice_(0), current_edge_addon_(nullptr)
+					choices_(choices), children_(children)
 				{}
 
 				void Begin() { choices_.Begin(); }
 				void StepNext() { choices_.StepNext(); }
 				bool IsEnd() { return choices_.IsEnd(); }
 
-				enum CheckResult {
-					kForceSelectChoice,
-					kNormalChoice
-				};
-				CheckResult Check() {
-					current_choice_ = choices_.Get();
-					assert(current_choice_ >= 0);
-
-					current_edge_addon_ = children_.Get(current_choice_).first;
-					return CheckChild(current_edge_addon_);
-				}
-				static CheckResult CheckChild(mcts::selection::EdgeAddon const* edge) {
-					if (!edge) return kForceSelectChoice; // not expanded before
-					return kNormalChoice;
-				}
-
-				int GetChoice() const { return current_choice_; }
-				mcts::selection::EdgeAddon const& GetAddon() const {
-					return *current_edge_addon_;
+				void Get(Item & item) {
+					item.choice = choices_.Get();
+					assert(item.choice >= 0);
+					item.edge_addon = children_.Get(item.choice).first;
 				}
 
 			private:
 				engine::ActionChoices & choices_;
 				mcts::selection::ChildNodeMap const& children_;
-
-				int current_choice_;
-				mcts::selection::EdgeAddon const* current_edge_addon_;
 			};
 
 			class RandomPolicy {
@@ -88,54 +74,45 @@ namespace mcts
 			public:
 				static constexpr double kExploreWeight = 0.8;
 
-				template <typename ChoiceIterator>
-				int SelectChoice(ChoiceIterator && choice_iterator)
+				int SelectChoice(ChoiceIterator choice_iterator)
 				{
-					struct Item {
-						int choice;
-						mcts::selection::EdgeAddon const* edge_addon;
-					};
 					constexpr size_t kMaxChoices = engine::IActionParameterGetter::kMaxChoices;
-					std::array<Item, kMaxChoices> choices;
+					std::array<ChoiceIterator::Item, kMaxChoices> choices;
 					size_t choices_size = 0;
 
 					// Phase 1: get total chosen times, and record to 'choices'
 					std::int64_t total_chosen_times = 0;
-					for (choice_iterator.Begin();
-						!choice_iterator.IsEnd();
-						choice_iterator.StepNext())
+					for (choice_iterator.Begin(); !choice_iterator.IsEnd(); choice_iterator.StepNext())
 					{
-						typename ChoiceIterator::CheckResult check_result = choice_iterator.Check();
+						choice_iterator.Get(choices[choices_size]);
+						auto & item = choices[choices_size];
+						int choice = item.choice;
+						auto edge_addon = item.edge_addon;
 
-						int choice = choice_iterator.GetChoice();
-						if (check_result == ChoiceIterator::CheckResult::kForceSelectChoice) {
-							return choice;
-						}
+						if (!edge_addon) return choice; // force select
 
-						auto const& edge_addon = choice_iterator.GetAddon();
-						auto chosen_times = edge_addon.GetChosenTimes();
+						auto chosen_times = edge_addon->GetChosenTimes();
 						if (chosen_times == 0) {
 							return choice; // force select
 						}
-						if (edge_addon.GetTotal() == 0) {
+						if (edge_addon->GetTotal() == 0) {
 							// a node is created (from another thread),
 							// but is not yet updated from that thread
 							// in this case, we just force select that choice
 							return choice;
 						}
 
-						assert(chosen_times > 0); // == 0
+						assert(chosen_times > 0);
 						total_chosen_times += chosen_times;
 
 						assert(choices_size < kMaxChoices);
-						choices[choices_size] = Item{ choice, &edge_addon };
 						++choices_size;
 					}
 
 					assert(total_chosen_times > 0);
 
 					// Phase 2: use UCB to make a choice
-					auto get_score = [total_chosen_times](Item const& item) {
+					auto get_score = [total_chosen_times](ChoiceIterator::Item const& item) {
 						double exploit_score = item.edge_addon->GetAverageCredit();
 						assert(exploit_score >= 0.0);
 						assert(exploit_score <= 1.0);
