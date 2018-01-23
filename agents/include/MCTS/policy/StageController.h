16871c46c6196fe60f9a94ac8302b8cdd063f1b0
Shen-En Shih <petershih@google.com>
2018-01-24 01:52:58 +0800

    agents: MCTS: No need to wait simulations for cutoff

--- agents/include/MCTS/policy/StageController.h
+++ agents/include/MCTS/policy/StageController.h
@@ -8,7 +8,7 @@ namespace mcts {
 		public:
 			// Switch to simulation mode if node is chosen too few times
 			// This can lower down the rate we allocate new nodes
-			static constexpr int kSwitchToSimulationUnderChosenTimes = 10;
+			static constexpr int kSwitchToSimulationUnderChosenTimes = 1;
 
 			static bool SwitchToSimulation(bool new_node_created, std::int64_t last_node_chosen_times) {
 				if (new_node_created) return true;
